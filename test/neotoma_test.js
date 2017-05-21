22fcd26f2d9ff55c8c2942c0945133eab497752f
SimonGoring <simon.j.goring@gmail.com>
2017-05-21 14:15:10 -0700

    Adding Testing Units
    There is now a testing unit, using packages `mocha`, `chai` and `supertest`.
    
    I'm working through the tutorial, but it seems to work okay.  Right now testing is implemented only for 200 tests.

---
+++ test/neotoma_test.js
@@ -0,0 +1,31 @@
+var should = require('chai').should();
+var expect = require('chai').expect;
+var supertest = require('supertest');
+var api = supertest('http://localhost:3000/');
+
+// *************************************************
+// Testing 200 responses:
+//
+it('should return a 200 response', function(done) {
+	api.get('/v2/data/geopoliticalunits/')
+	.set('Accept', 'application/json')
+	.expect(200, done);
+})
+
+it('should return a 200 response', function(done) {
+	api.get('/v2/data/geopoliticalunits/12')
+	.set('Accept', 'application/json')
+	.expect(200, done);
+})
+
+it('should return a 200 response', function(done) {
+	api.get('/v2/data/publications')
+	.set('Accept', 'application/json')
+	.expect(200, done);
+})
+
+it('should return a 200 response', function(done) {
+	api.get('/v2/data/publications/12')
+	.set('Accept', 'application/json')
+	.expect(200, done);
+})
