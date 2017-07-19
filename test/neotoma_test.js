f1952b7baa0fb8d84805631e8032d4b5124a65f5
SimonGoring <simon.j.goring@gmail.com>
2017-07-19 10:39:09 -0700

    Added the `neotoma_test` units.  Still just a template.

--- test/neotoma_test.js
+++ test/neotoma_test.js
@@ -6,6 +6,7 @@ var api = supertest('http://localhost:3000/');
 // *************************************************
 // Geopolitical Units:
 //
+
 it('should return a 200 response', function(done) {
 	api.get('/v2/data/geopoliticalunits/')
 	.set('Accept', 'application/json')
