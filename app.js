4b959341ce7a89728882bcc24f3072c8c2942f47
SimonGoring <simon.j.goring@gmail.com>
2017-08-18 10:42:56 -0700

    Just some weird formatting stuff for the `app.js` file.  Playing with whitespace.
    Formatting with the `gpubyid` file as well.
    
    The `datasets` files have all been moved into their own folders too.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+     var express = require('express');
+        var path = require('path');
+     var favicon = require('serve-favicon');
+      var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+  var bodyParser = require('body-parser');
 var swaggerJSDoc = require('swagger-jsdoc');
 
 const querystring = require('querystring');
