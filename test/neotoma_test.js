2c9b3faaf172ec14215784f833c2c1609af5ea87
SimonGoring <simon.j.goring@gmail.com>
2017-09-27 12:09:43 -0700

    This change results in new tests in `neotoma_test` and the addition of limit/offset terms in the queries.
    It also results in the addition of new functionality, wherein an empty query re-directs users to the swagger documentation.

--- test/neotoma_test.js
+++ test/neotoma_test.js
@@ -1,3 +1,4 @@
+var assert = require('assert');
 var should = require('chai').should();
 var expect = require('chai').expect;
 var supertest = require('supertest');
@@ -9,22 +10,37 @@ var api = supertest('http://localhost:3000/');
 
 describe('Get geopolitical data:', function(){
 	this.timeout(5000);  // takes a while to run.
-	it('A number of geopolitical units should be returned.', function(done) {
+	it('An empty query redirects to the api documentation.', function(done) {
 		api.get('v2/data/geopoliticalunits/')
 		.set('Accept', 'application/json')
-		.expect(function(res){
-			Object.keys(res).length > 10;
-		})
-		.expect(200, done)
+		.expect(302, done);
+	});
+
+	it('The limit should be reached for country level data:', function(done) {
+		api.get('v2/data/geopoliticalunits/?rank=1')
+		.set('Accept', 'application/json')
+		.end(function(err, res){
+			assert.equal(Object.keys(res.body.data.result).length, 25);
+			done();
+		});
+	});
+
+	it('Changing the limit should change the number of countries retrieved:', function(done) {
+		api.get('v2/data/geopoliticalunits/?rank=1&limit=30')
+		.set('Accept', 'application/json')
+		.end(function(err, res){
+			assert.equal(Object.keys(res.body.data.result).length, 30);
+			done();
+		});
 	});
 	
 	it('A single geopolitical unit (12) should be returned.', function(done) {
-	api.get('v2/data/geopoliticalunits/12')
+		api.get('v2/data/geopoliticalunits/12')
 		.set('Accept', 'application/json')
-		.expect(function(res){
-			return res.body['data'][0]['geopoliticalid'] === 12;
-		})
-		.expect(200, done);
+		.end(function(err, res){
+			assert.equal(res.body.data[0]['geopoliticalid'], 12);
+			done();
+		});
 	});
 
 });
