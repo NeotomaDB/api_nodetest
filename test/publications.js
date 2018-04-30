a40080d06b1e30f637dc7d76b0b9d41e702d1c0e
SimonGoring <simon.j.goring@gmail.com>
2018-04-30 01:18:33 -0700

    # Fixing the publication tests
    Working now on the publications.  Generally have things working in v2.0.

--- test/publications.js
+++ test/publications.js
@@ -8,45 +8,18 @@ var api = supertest('http://localhost:3000/');
 // Site Data:
 //
 
-describe('Get publication data any number of ways:', function(){
-	
-	this.timeout(15000);
+describe('Get publication data any number of ways:', function () {
+  this.timeout(15000);
 
-	it('Get publication by singular id & return same id:', function(done) {
-		api.get('v2/data/publications/12')
-		.set('Accept', 'application/json')
-		.expect(function(res){
-				return Object.keys(res.body['data'][0]).length > 0;
-			})
-		.expect(function(res){
-				return res.body['data'][0]['publicationid'] === 12;
-			})
-		.expect(200, done);
-	});
-	it('Get site by altitude:', function(done) {
-		api.get('v2/data/sites/?altmax=5000&altmin=3000')
-		.set('Accept', 'application/json')
-		.expect(function(res){
-				return Object.keys(res.body['data'][0]).length > 0;
-			})
-		.expect(200, done);
-	});
-	it('Break sites by flipping altitudes:', function(done) {
-		api.get('v2/data/sites/?altmax=3000&altmin=5000')
-		.set('Accept', 'application/json')
-		.expect(500, done);
-	});
-	it('Break sites by passing invalid siteid:', function(done) {
-		api.get('v2/data/sites/abcd')
-		.set('Accept', 'application/json')
-		.expect(500, done);
-	});
-	it('Get site by geopolitical units returns gp and site data:', function(done) {
-		api.get('v2/data/geopoliticalunits/765/sites')
-		.set('Accept', 'application/json')
-		.expect(function(res){
-				return Object.keys(res.body['data'][0]).length === 2;
-			})
-		.expect(200, done);
-	});
+  it('Get publication by singular id & return same id:', function (done) {
+    api.get('v2.0/data/publications/12')
+      .set('Accept', 'application/json')
+      .expect(function (res) {
+        return Object.keys(res.body['data'][0]).length > 0;
+      })
+      .expect(function (res) {
+        return res.body['data'][0]['publicationid'] === 12;
+      })
+      .expect(200, done);
+  });
 });
