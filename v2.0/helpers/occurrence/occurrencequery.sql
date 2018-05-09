804364ebb92a45b3481ae0de1f8b16abed5735df
Simon <simon.j.goring@gmail.com>
2018-05-09 15:29:30 -0700

    Occurrence support for WKT and GeoJSON
    These changes now allow a user to post either geoJSON or WKT strings for the geometry paths.

--- v2.0/helpers/occurrence/occurrencequery.sql
+++ v2.0/helpers/occurrence/occurrencequery.sql
@@ -20,9 +20,9 @@ SELECT
 	LEFT JOIN       ndb.variables AS var     ON data.variableid = var.variableid
 	LEFT JOIN   ndb.variableunits AS varu    ON var.variableunitsid  = varu.variableunitsid
 	LEFT JOIN  ndb.samplekeywords AS sampkey ON samples.sampleid = sampkey.sampleid
+	LEFT JOIN         ndb.dslinks AS links   ON samples.datasetid = links.datasetid
 	LEFT JOIN        ndb.datasets AS ds      ON samples.datasetid = ds.datasetid 
-	LEFT JOIN ndb.collectionunits AS cu      ON ds.collectionunitid = cu.collectionunitid
-	LEFT JOIN           ndb.sites AS sts     ON cu.siteid = sts.siteid
+	LEFT JOIN           ndb.sites AS sts     ON links.siteid = sts.siteid
 	LEFT JOIN      ndb.sampleages AS ages    ON ages.sampleid = samples.sampleid
 	LEFT JOIN            ndb.taxa AS tx      ON var.taxonid = tx.taxonid
 	LEFT JOIN    ndb.datasettypes AS dt      ON ds.datasettypeid = dt.datasettypeid
@@ -33,12 +33,12 @@ WHERE
 	tx.taxonname IS NOT NULL AND
 	(${taxonname} IS NULL OR tx.taxonname LIKE ${taxonname}) AND
 	(${taxonid} IS NULL OR var.taxonid IN (${taxonid:csv})) AND
-	(${siteid} IS NULL OR sts.siteid IN (${siteid:csv})) AND
+	(${siteid} IS NULL OR links.siteid IN (${siteid:csv})) AND
 	(${sitename} IS NULL OR sts.sitename LIKE ${sitename}) AND
  	(${datasettype} IS NULL OR dt.datasettype LIKE ${datasettype}) AND
  	(${altmin} IS NULL OR sts.altitude > ${altmin}) AND
 	(${altmax} IS NULL OR sts.altitude > ${altmax}) AND
-	(${loc} IS NULL OR st_contains(ST_GeomFromText(${loc}), sts.geom)) AND
+	(${loc} IS NULL OR st_contains(ST_SetSRID(ST_GeomFromText(${loc}), 4326), sts.geom)) AND
 	(${ageyoung} IS NULL OR 
 		CASE WHEN ages.ageyounger IS NOT NULL 
 		     THEN (ages.ageyounger > ${ageyoung})
