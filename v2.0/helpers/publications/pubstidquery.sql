4f60378905d1394cc4ee7750b7b8cde6ef1d59f7
Simon <simon.j.goring@gmail.com>
2018-05-09 10:42:40 -0700

    Fixed up the site search for publications
    Publications now hits site publications as well.

--- v2.0/helpers/publications/pubstidquery.sql
+++ v2.0/helpers/publications/pubstidquery.sql
@@ -1,11 +1,14 @@
-WITH dpub AS 
-  (SELECT * FROM 
-  	ndb.datasetpublications as dp 
-    WHERE ($1 IS NULL OR dp.datasetid IN ($1:csv)))
+WITH stpub AS 
+  (SELECT publicationid FROM 
+  	ndb.datasetpublications as dp
+  	INNER JOIN
+  	ndb.dslinks AS sts
+  	ON sts.datasetid = dp.datasetid
+    WHERE ($1 IS NULL OR sts.siteid IN ($1:csv)))
 SELECT * FROM 
           ndb.publications AS pub 
   INNER JOIN
     ndb.publicationauthors AS pa ON pub.publicationid = pa.publicationid 
   INNER JOIN 
     ndb.contacts as ca ON ca.contactid = pa.contactid 
-WHERE pub.publicationid IN (SELECT publicationid FROM dpub)
+WHERE pub.publicationid IN (SELECT publicationid FROM stpub)
