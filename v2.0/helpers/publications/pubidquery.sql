ff5420d14d0b11d4022209435f94439565696f95
Simon <simon.j.goring@gmail.com>
2018-05-09 11:32:13 -0700

    Fixed the datajs file to reference new funcitons.

--- v2.0/helpers/publications/pubidquery.sql
+++ v2.0/helpers/publications/pubidquery.sql
@@ -1,11 +1,8 @@
-WITH dpub AS 
-  (SELECT * FROM 
-  	ndb.datasetpublications as dp 
-    WHERE ($1 IS NULL OR dp.datasetid IN ($1:csv)))
 SELECT * FROM 
           ndb.publications AS pub 
-  INNER JOIN
+INNER JOIN
     ndb.publicationauthors AS pa ON pub.publicationid = pa.publicationid 
-  INNER JOIN 
+INNER JOIN 
     ndb.contacts as ca ON ca.contactid = pa.contactid 
-WHERE pub.publicationid IN (SELECT publicationid FROM dpub)
+WHERE
+  (${pubid} IS NULL OR pub.pubid = ${pubid})
