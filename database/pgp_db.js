22f7b72e89c2743f33dfa8962b5b0d9eb3e2c9e4
Simon <simon.j.goring@gmail.com>
2018-05-11 02:41:55 -0700

    Minor changes to a couple files.

--- database/pgp_db.js
+++ database/pgp_db.js
@@ -10,10 +10,9 @@ const ctStr = require('./db_connect.json');
 
 const db = pgp(ctStr);
 
-
 db.proc('version')
   .then(data => {
-    //console.log(data.version);
+    // console.log(data.version);
   })
   .catch(error => {
     console.log("error connecting");
