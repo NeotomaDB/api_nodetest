e84b236594c1aaf2cc6c83dc47137cbe70a34228
Simon <simon.j.goring@gmail.com>
2018-07-11 14:57:28 -0700

    Fixing issue #64, problem was curious, result of the use of inner join where data was missing in one table.

--- v2.0/helpers/publications/pubquery.sql
+++ v2.0/helpers/publications/pubquery.sql
@@ -1,20 +1,17 @@
-SELECT DISTINCT pub.* FROM 
-  ndb.publications AS pub 
+SELECT DISTINCT pub.* FROM
+  ndb.publications AS pub
 INNER JOIN
-    ndb.publicationauthors AS pa ON pub.publicationid = pa.publicationid
-INNER JOIN 
-    ndb.contacts as ca ON ca.contactid = pa.contactid 
+     ndb.publicationauthors AS pa  ON pub.publicationid = pa.publicationid
 INNER JOIN
-    ndb.datasetpublications as dp ON dp.publicationid = pub.publicationid
+               ndb.contacts AS ca  ON      ca.contactid = pa.contactid
+LEFT JOIN
+    ndb.datasetpublications AS dp  ON  dp.publicationid = pub.publicationid
 INNER JOIN
-    ndb.dslinks AS dsl ON dp.datasetid = dsl.datasetid
-INNER JOIN
-	ndb.publicationtypes AS pt ON pub.pubtypeid = pt.pubtypeid
+	     ndb.publicationtypes AS pt  ON     pub.pubtypeid = pt.pubtypeid
 WHERE
-  (${pubid} IS NULL OR pub.publicationid = ANY (${pubid})) AND
-  (${datasetid} IS NULL OR dp.datasetid = ANY (${datasetid})) AND
-  (${familyname} IS NULL OR ca.familyname LIKE ${familyname}) AND
-  (${pubtype} IS NULL OR pt.pubtype = ${pubtype}) AND
-  (${year} IS NULL OR pub.year = ${year}) AND
-  (${search} IS NULL OR pub.citation SIMILAR TO (${search}))
-GROUP BY pub.publicationid
+  (${pubid}      IS NULL OR pub.publicationid = ANY (${pubid}::int[]))     AND
+  (${datasetid}  IS NULL OR      dp.datasetid = ANY (${datasetid})) AND
+  (${familyname} IS NULL OR     ca.familyname LIKE  ${familyname})  AND
+  (${pubtype}    IS NULL OR        pt.pubtype =     ${pubtype})     AND
+  (${year}       IS NULL OR          pub.year =     ${year})        AND
+  (${search}     IS NULL OR      pub.citation SIMILAR TO (${search}))
