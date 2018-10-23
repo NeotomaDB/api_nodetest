2774a9495edce11fb5f50fdb16825d46f057b2e5
Simon <simon.j.goring@gmail.com>
2018-10-23 10:35:25 -0700

    Adding CORS support for apidev.

--- app.js
+++ app.js
@@ -101,6 +101,12 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.all('*', function (req, res) {
   res.redirect('/api-docs');
 });
