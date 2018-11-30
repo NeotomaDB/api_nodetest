3ec8d3dc68aa0e1e2f5dc5260ba29f16b6aeecbe
Simon <simon.j.goring@gmail.com>
2018-11-30 11:13:49 -0800

    Adding the email to contact responses.

--- v2.0/helpers/contacts/contactbyid.sql
+++ v2.0/helpers/contacts/contactbyid.sql
@@ -1,10 +1,11 @@
-SELECT 
+SELECT
   cnt.contactid AS contactid,
   cnt.contactname AS fullName,
-  cnt.familyname AS lastName, 
+  cnt.familyname AS lastName,
   cnt.givennames AS firstName,
   cnt.url AS url,
+  cnt.email AS email,
   cnt.address AS address
 FROM
-  ndb.contacts AS cnt 
+  ndb.contacts AS cnt
 WHERE cnt.contactid IN ($1:csv);
