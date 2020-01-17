56aef234e8a3264f5784e9247fedee830b55db5c
Simon <simon.j.goring@gmail.com>
2020-01-17 12:58:09 -0800

    More detailled chronology returns.

--- v2.0/helpers/chronology/chronology.js
+++ v2.0/helpers/chronology/chronology.js
@@ -27,6 +27,7 @@ const chronologybydsidsql = sql('./chronologybydsid.sql');
 const chronologybystidsql = sql('./chronologybystid.sql');
 
 function chronologybyid (req, res, next) {
+
   if (!(req.params.chronologyid == null)) {
     var chronologyid = String(req.params.chronologyid).split(',').map(function (item) {
       return parseInt(item, 10);
@@ -42,27 +43,10 @@ function chronologybyid (req, res, next) {
 
   db.any(chronologybyidsql, [chronologyid])
     .then(function (data) {
-      for (var i = 0; i < data.length; i++) {
-        var refObj = data[i].chronology.controls.reduce((acc, item) => {
-          if (!acc[item.datasetid]) {
-            acc[item.datasetid] = {
-              'datasetid': item.datasetid,
-              'datasettype': item.datasettype,
-              'controls': [] };
-          };
-
-          acc[item.datasetid].controls.push(item.controls.chroncontrols);
-
-          return acc;
-        }, {});
-
-        data[i].chronology.controls = refObj;
-      }
-
       res.status(200)
         .json({
           status: 'success',
-          data: removeEmpty(data),
+          data: data,
           message: 'Retrieved all tables'
         });
     })
@@ -90,78 +74,10 @@ function chronologybydsid (req, res, next) {
 
   db.any(chronologybydsidsql, [datasetid])
     .then(function (data) {
-      var dataSets = datasetid.map(function (x) {
-        var out = { datasetid: x }
-        return out;
-      })
-
-      for (var i = 0; i < dataSets.length; i++) {
-        /* Get all objects (rows) in data that are associated with the dataset: */
-        var newchrons = data.filter(obj => {
-          return obj.datasetid === dataSets[i]['datasetid']
-        })
-
-        var dschrons = newchrons.map(obj => obj['chronologyid']).filter((x, i, a) => a.indexOf(x) === i)
-
-        if (dschrons.length > 0) {
-          /* Get the unique chronologies for the dataset and create objects */
-          dataSets[i]['chronologies'] = dschrons.map(function (x) {
-            return { chronologyid: x }
-          })
-
-          for (var j = 0; j < dschrons.length; j++) {
-            var chronset = data.filter(obj => {
-              return obj.datasetid === dataSets[i]['datasetid'] & obj.chronologyid === dataSets[i]['chronologies'][j]['chronologyid']
-            })
-
-            /* var ccrid = chronset.map(x => x['chroncontrolid']).filter((x, i, a) => a.indexOf(x) === i) */
-
-            dataSets[i]['chronologies'][j] = { 'chronologyid': chronset[0]['chronologyid'],
-              agetype: chronset[0]['modelagetype'],
-              agemodel: chronset[0]['agemodel'],
-              isdefault: chronset[0]['isdefault'],
-              chronologyname: chronset[0]['chronologyname'],
-              dateprepared: chronset[0]['dateprepared'],
-              chronologynotes: chronset[0]['chronologynotes'],
-              chronologyagepan: { younger: chronset[0]['ageboundyounger'],
-                older: chronset[0]['ageboundolder'] },
-              preparedby: {
-                contactid: chronset[0]['contactid'],
-                familyname: chronset[0]['familyname'],
-                firstname: chronset[0]['givennames'],
-                initials: chronset[0]['leadinginitials'] },
-              controls: chronset.map(function (x) {
-                var out = { 'chroncontrolid': x['chroncontrolid'],
-                  'depth': x['depth'],
-                  'thickness': x['thickness'],
-                  'age': x['ccage'],
-                  'agelimityounger': x['agelimityounger'],
-                  'agelimitolder': x['agelimitolder'],
-                  'chroncontroltype': x['chroncontroltype'],
-                  'geochron': {
-                    'geochronid': x['geochronid'],
-                    'labnumber': x['labnumber'],
-                    'geochrontype': x['geochrontype'],
-                    'age': x['gcage'],
-                    'errorolder': x['errorolder'],
-                    'erroryounger': x['erroryounger'],
-                    'infinite': x['infinite'],
-                    'delta13c': x['delta13c'],
-                    'agetype': x['geochronagetype'],
-                    'notes': x['gcnotes'],
-                    'materialdated': x['geochronagetype']
-                  }
-                }
-                return out;
-              })
-            }
-          }
-        }
-      }
       res.status(200)
         .json({
           status: 'success',
-          data: dataSets,
+          data: data,
           message: 'Retrieved all tables'
         });
     })
--- v2.0/helpers/chronology/chronologybydsid.sql
+++ v2.0/helpers/chronology/chronologybydsid.sql
@@ -1,48 +1,65 @@
 SELECT
-  dts.datasetid,
-  chrs.chronologyid,
-  aty.agetype  AS modelagetype,
-  chrs.isdefault,
-  chrs.chronologyname,
-  chrs.dateprepared,
-  chrs.agemodel,
-  chrs.ageboundyounger,
-  chrs.ageboundolder,
-  chrs.notes,
-  cnt.contactid,
-  cnt.contactname,
-  cnt.familyname,
-  cnt.givennames,
-  cnt.leadinginitials,
-  chctrl.chroncontrolid,
-  chctrl.depth,
-  chctrl.thickness,
-  chctrl.age AS ccage,
-  chctrl.agelimityounger,
-  chctrl.agelimitolder,
-  chty.chroncontroltype,
-  gc.geochronid,
-  gc.labnumber,
-  gct.geochrontype,
-  gc.age AS gcage,
-  gc.errorolder,
-  gc.erroryounger,
-  gc.infinite,
-  gc.delta13c,
-  atyg.agetype AS geochronagetype,
-  gc.notes as gcnotes,
-  gc.materialdated
-FROM                   ndb.chronologies AS chrs
-  LEFT OUTER JOIN     ndb.chroncontrols AS chctrl ON chrs.chronologyid = chctrl.chronologyid
-  LEFT OUTER JOIN ndb.chroncontroltypes AS chty   ON chctrl.chroncontroltypeid = chty.chroncontroltypeid
-  LEFT OUTER JOIN           ndb.dslinks AS dsl    ON chrs.collectionunitid = dsl.collectionunitid
-  LEFT OUTER JOIN          ndb.agetypes AS aty    ON chrs.agetypeid = aty.agetypeid
-  LEFT OUTER JOIN          ndb.datasets AS dts    ON dsl.datasetid = dts.datasetid
-  LEFT OUTER JOIN      ndb.datasettypes AS dty    ON dts.datasettypeid = dty.datasettypeid
-  LEFT OUTER JOIN          ndb.contacts AS cnt    ON cnt.contactid = chrs.contactid
-  LEFT OUTER JOIN  ndb.geochroncontrols AS gcc    ON gcc.chroncontrolid = chctrl.chroncontrolid
-  LEFT OUTER JOIN     ndb.geochronology AS gc     ON gc.geochronid = gcc.geochronid
-  LEFT OUTER JOIN     ndb.geochrontypes AS gct    ON gct.geochrontypeid = gc.geochrontypeid
-  LEFT OUTER JOIN          ndb.agetypes AS atyg   ON atyg.agetypeid = gc.agetypeid
+jsonb_build_object(   'chronologyid', chrs.chronologyid,
+                          'agetype', aty.agetype,
+                        'modelType', chrs.agemodel,
+                          'default', chrs.isdefault,
+                   'chronologyName', chrs.chronologyname,
+                     'datePrepared', chrs.dateprepared,
+                'reliableagespan', json_build_object('younger', chrs.ageboundyounger,
+                                                         'older', chrs.ageboundolder),
+                  'chronologynotes', chrs.notes,
+                       'preparedby', json_build_object('contactid', cnt.contactid,
+                                                     'contactname', cnt.contactname,
+                                                      'familyname', cnt.familyname,
+                                                       'firstname', cnt.givennames,
+                                                        'initials', cnt.leadinginitials),
+         'datasets', json_agg(DISTINCT jsonb_build_object('datasetid', dts.datasetid,
+                                          'datasettype', dty.datasettype)),
+          'controls', json_agg(DISTINCT jsonb_build_object(
+                                                        'chroncontrolid', chctrl.chroncontrolid,
+                                                        'analysisunitid', au.analysisunitid,
+                                                        'analysisunitname', au.analysisunitname,
+                                                                 'depth', chctrl.depth,
+                                                             'thickness', chctrl.thickness,
+                                                               'agetype', atyct.agetype,
+                                                                   'age', chctrl.age,
+                                                            'controllimits', json_build_object('ageyounger', chctrl.agelimityounger,
+                                                                                                 'ageolder', chctrl.agelimitolder),
+                                                           'controltype', chty.chroncontroltype,
+                                                                 'notes', chctrl.notes,
+                                                              'geochron', jsonb_build_object(
+                                                                'geochronid', gc.geochronid,
+                                                                'geochronlabnumber', gc.labnumber,
+                                                                'geochrontype', gct.geochrontype,
+                                                                'geochronage', jsonb_build_object('age', gc.age,
+                                                                                                  'errorolder', gc.errorolder,
+                                                                                                  'erroryounger', gc.erroryounger),
+                                                                'infinite', gc.infinite,
+                                                                'delta13c', gc.delta13c,
+                                                                'geochronagetype', atyg.agetype,
+                                                                'notes', gc.notes,
+                                                                'materialdated', gc.materialdated),
+		  													'relativeage', jsonb_build_object('relativeage', ra.relativeage,
+																							  'c14ages', jsonb_build_object('c14ageyounger', ra.c14ageyounger,
+																														    'c14ageolder', ra.c14ageolder),
+																							  'calages', jsonb_build_object('calageyounger', ra.calageyounger,
+																														    'calageolder', ra.calageolder))))) AS chronology
+
+FROM                    ndb.chronologies AS chrs
+  LEFT OUTER JOIN      ndb.chroncontrols AS chctrl ON chrs.chronologyid = chctrl.chronologyid
+  LEFT OUTER JOIN  ndb.chroncontroltypes AS chty   ON chctrl.chroncontroltypeid = chty.chroncontroltypeid
+  LEFT OUTER JOIN            ndb.dslinks AS dsl    ON chrs.collectionunitid = dsl.collectionunitid
+  LEFT OUTER JOIN           ndb.agetypes AS aty    ON chrs.agetypeid = aty.agetypeid
+  LEFT OUTER JOIN           ndb.agetypes AS atyct  ON chctrl.agetypeid = aty.agetypeid
+  LEFT OUTER JOIN           ndb.datasets AS dts    ON dsl.datasetid = dts.datasetid
+  LEFT OUTER JOIN       ndb.datasettypes AS dty    ON dts.datasettypeid = dty.datasettypeid
+  LEFT OUTER JOIN           ndb.contacts AS cnt    ON cnt.contactid = chrs.contactid
+  LEFT OUTER JOIN   ndb.geochroncontrols AS gcc    ON gcc.chroncontrolid = chctrl.chroncontrolid
+  LEFT OUTER JOIN      ndb.geochronology AS gc     ON gc.geochronid = gcc.geochronid
+  LEFT OUTER JOIN      ndb.geochrontypes AS gct    ON gct.geochrontypeid = gc.geochrontypeid
+  LEFT OUTER JOIN           ndb.agetypes AS atyg   ON atyg.agetypeid = gc.agetypeid
+  LEFT OUTER JOIN ndb.relativechronology AS rc     ON rc.analysisunitid = chctrl.analysisunitid
+  LEFT OUTER JOIN       ndb.relativeages AS ra     ON ra.relativeageid = rc.relativeageid
+  LEFT JOIN       ndb.analysisunits AS au     ON au.analysisunitid = chctrl.analysisunitid
 WHERE    dts.datasetid IN ($1:csv)
-ORDER BY dts.datasetid, chrs.chronologyid, chctrl.chroncontrolid, gc.geochronid
+GROUP BY chrs.chronologyid, aty.agetype, cnt.contactid, au.analysisunitid
