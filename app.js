1b46b7bd31a0807777d6e8c350360405ef0bfdf9
Simon <simon.j.goring@gmail.com>
2020-02-28 09:50:21 -0800

    Added support for sites by geopolitical ID

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
+app.use(express.static('public'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
---
+++ v2.0/helpers/datasets/datasetbygpid.sql
@@ -0,0 +1,77 @@
+WITH RECURSIVE allgpu AS (SELECT gpu.geopoliticalid, gpu.highergeopoliticalid
+                               FROM ndb.geopoliticalunits AS gpu
+                              WHERE gpu.geopoliticalid IN ($1:csv)
+                              UNION ALL
+                             SELECT m.geopoliticalid, m.highergeopoliticalid
+                               FROM ndb.geopoliticalunits AS m
+                               JOIN allgpu ON allgpu.geopoliticalid = m.highergeopoliticalid),
+     dspiagg AS (
+  SELECT
+	dts.datasetid AS datasetid,
+	sts.siteid AS siteid,
+	clu.collectionunitid,
+    jsonb_build_object('datasetid', dts.datasetid,
+                           'datasettype', dst.datasettype,
+                           'datasetnotes', dts.notes,
+                           'database', cstdb.databasename,
+                           'doi', json_agg(DISTINCT doi.doi),
+                           'datasetpi', json_agg(json_build_object('contactid', cnt.contactid,
+                                                                'contactname', cnt.contactname,
+                                                                'familyname', cnt.familyname,
+                                                                'firstname', cnt.givennames,
+                                                                'initials', cnt.leadinginitials)),
+                                 'agerange', json_build_object('ageyoung', agerange.younger,
+                                                               'ageold', agerange.older,
+                                                               'units', agetypes.agetype))
+                                 AS dataset
+	FROM
+	ndb.datasets AS dts
+	LEFT OUTER JOIN ndb.collectionunits      AS clu      ON clu.collectionunitid = dts.collectionunitid
+	LEFT OUTER JOIN ndb.datasettypes         AS dst      ON dst.datasettypeid = dts.datasettypeid
+  LEFT OUTER JOIN ndb.datasetdoi           AS doi      ON dts.datasetid = doi.datasetid
+	LEFT OUTER JOIN ndb.datasetdatabases     AS dsdb     ON dsdb.datasetid = dts.datasetid
+	LEFT OUTER JOIN ndb.datasetpis           AS dspi     ON dspi.datasetid = dts.datasetid
+	LEFT OUTER JOIN ndb.contacts             AS cnt      ON cnt.contactid = dspi.contactid
+	LEFT OUTER JOIN ndb.dsageranges          AS agerange ON dts.datasetid = agerange.datasetid
+	LEFT OUTER JOIN ndb.agetypes             AS agetypes ON agetypes.agetypeid = agerange.agetypeid
+	LEFT OUTER JOIN ndb.constituentdatabases AS cstdb    ON dsdb.databaseid = cstdb.databaseid
+	LEFT OUTER JOIN ndb.sites                AS sts      ON sts.siteid = clu.siteid
+  WHERE dts.datasetid = ANY ($1)
+  GROUP BY
+	  sts.siteid,
+    clu.collectionunitid,
+    dts.datasetid,
+    dst.datasettype,
+    cstdb.databasename,
+    agerange.younger,
+    agerange.older,
+    agetypes.agetype
+)
+
+SELECT  json_build_object('geopoliticalid', gpu.geopoliticalid,
+                         'geopoliticalname', gpu.geopoliticalname,
+                         'geopoliticalunit', gpu.geopoliticalunit,
+                         'rank', gpu.rank,
+                         'highergeopoliticalid', gpu.highergeopoliticalid) AS geopoliticalunit,
+        json_build_object(       'siteid', sts.siteid,
+                              'sitename', sts.sitename,
+                       'sitedescription', sts.sitedescription,
+                             'sitenotes', sts.notes,
+                             'geography', ST_AsGeoJSON(sts.geog,5,2),
+                              'altitude', sts.altitude,
+                      'collectionunitid', clu.collectionunitid,
+                        'collectionunit', clu.collunitname,
+                                'handle', clu.handle,
+                              'unittype', cts.colltype,
+						'datasets', json_agg(dspi.dataset)) as site
+FROM ndb.datasets AS dts
+  LEFT OUTER JOIN dspiagg             AS dspi ON dspi.datasetid = dts.datasetid
+  LEFT OUTER JOIN ndb.collectionunits AS clu  ON clu.collectionunitid = dts.collectionunitid
+  LEFT OUTER JOIN ndb.sites           AS sts  ON sts.siteid = clu.siteid
+  LEFT OUTER JOIN ndb.collectiontypes as cts  ON clu.colltypeid = cts.colltypeid
+WHERE dts.datasetid = ANY ($1)
+GROUP BY
+  sts.siteid,
+  clu.collectionunitid,
+  cts.colltype,
+  dspi.dataset;
