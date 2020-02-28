eef453b44448c078b5a5c36503e33fef90bd4d3c
Simon <simon.j.goring@gmail.com>
2020-02-28 11:59:37 -0800

    Updating the `sitebyid` query and swagger to match the new `sites` frmt.

--- v2.0/helpers/sites/sitebyid.sql
+++ v2.0/helpers/sites/sitebyid.sql
@@ -1,19 +1,29 @@
-SELECT json_build_object('siteid', sts.siteid,
-                       'sitename', sts.sitename, 
-                       'altitude', sts.altitude,
-                       'geography', ST_AsGeoJSON(sts.geog,5,2),
-                       'description', sts.sitedescription) AS site,
-       json_agg(json_build_object('collectionunitid', clu.collectionunitid,
-                                  'collectionunit', clu.collunitname,
-                                  'handle', clu.handle,
-                                  'unittype', cts.colltype,
-                                  'datasetid', dts.datasetid,
-                                  'datasettype', dst.datasettype)) AS dataset
+WITH collu AS (
+	SELECT sts.siteid,
+	       json_build_object('collectionunitid', clu.collectionunitid,
+                             'collectionunit', clu.collunitname,
+                                     'handle', clu.handle,
+                                   'collectionunittype', cts.colltype,
+                                    'datasets', json_agg(json_build_object('datasetid', dts.datasetid,
+                                                                        'datasettype', dst.datasettype))) AS collectionunit
+	FROM
+	ndb.datasets AS dts LEFT OUTER JOIN
+	ndb.collectionunits AS clu ON clu.collectionunitid = dts.collectionunitid LEFT OUTER JOIN
+	ndb.sites AS sts ON sts.siteid = clu.siteid  LEFT OUTER JOIN
+	ndb.datasettypes AS dst ON dst.datasettypeid = dts.datasettypeid LEFT OUTER JOIN
+	ndb.collectiontypes as cts ON clu.colltypeid = cts.colltypeid WHERE
+	sts.siteid IN ($1:csv)
+	GROUP BY sts.siteid, clu.collectionunitid, cts.colltype
+)
+SELECT sts.siteid,
+        sts.sitename,
+        sts.altitude,
+        ST_AsGeoJSON(sts.geog,5,2) AS geography,
+        sts.sitedescription,
+		json_agg(collu.collectionunit) AS collectionunits
 FROM
-ndb.datasets AS dts LEFT OUTER JOIN
-ndb.collectionunits AS clu ON clu.collectionunitid = dts.collectionunitid LEFT OUTER JOIN
-ndb.sites AS sts ON sts.siteid = clu.siteid  LEFT OUTER JOIN
-ndb.datasettypes AS dst ON dst.datasettypeid = dts.datasettypeid LEFT OUTER JOIN
-ndb.collectiontypes as cts ON clu.colltypeid = cts.colltypeid WHERE
+	ndb.sites AS sts
+	LEFT OUTER JOIN collu AS collu ON collu.siteid = sts.siteid
+WHERE
 sts.siteid IN ($1:csv)
 GROUP BY sts.siteid;
