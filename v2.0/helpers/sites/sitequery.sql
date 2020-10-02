d2b1f9cc312ee93f5be4dada031cc2aef20a165b
Simon Goring <goring@wisc.edu>
2020-10-02 15:21:30 -0700

    Working to clean the site query structure.

--- v2.0/helpers/sites/sitequery.sql
+++ v2.0/helpers/sites/sitequery.sql
@@ -1,26 +1,40 @@
-WITH sitid AS
-(SELECT * FROM ndb.sitegeopolitical AS sgp WHERE
-(${gpid} IS NULL OR sgp.geopoliticalid = ${gpid}))
-SELECT sts.siteid AS siteid,
+WITH sitid AS (
+  SELECT *
+  FROM
+    ndb.sitegeopolitical AS sgp
+  WHERE
+    (${gpid} IS NULL OR sgp.geopoliticalid = ${gpid})
+),
+collunit AS (
+	SELECT sts.siteid,
+		   json_build_object('collectionunitid', clu.collectionunitid,
+							 'collectionunit', clu.collunitname,
+									 'handle', clu.handle,
+						 'collectionunittype', cts.colltype,
+								   'datasets', json_agg(json_build_object('datasetid', dts.datasetid,
+																		'datasettype', dst.datasettype))) AS collectionunit
+	FROM
+		 ndb.datasets AS dts
+		 LEFT JOIN ndb.collectionunits AS clu ON clu.collectionunitid = dts.collectionunitid
+		 LEFT JOIN           ndb.sites AS sts ON           sts.siteid = clu.siteid
+		 LEFT OUTER JOIN    ndb.datasettypes AS dst ON    dst.datasettypeid = dts.datasettypeid
+		 LEFT OUTER JOIN ndb.collectiontypes AS cts ON       clu.colltypeid = cts.colltypeid
+	WHERE
+	  (${sitename} IS NULL OR sts.sitename ILIKE ${sitename})
+	  AND (${altmin} IS NULL OR sts.altitude >= ${altmin})
+	  AND (${altmax} IS NULL OR sts.altitude <= ${altmax})
+	  AND (${loc}    IS NULL OR ST_Intersects(ST_GeogFromText(${loc}), sts.geog))
+	  AND (${siteid} IS NULL OR sts.siteid IN ${siteid})
+	  AND sts.siteid IN (SELECT siteid FROM sitid)
+	GROUP BY sts.siteid, clu.collectionunitid, cts.colltype
+)
+SELECT sts.siteid,
        sts.sitename as sitename,
        sts.sitedescription AS sitedescription,
        ST_AsGeoJSON(sts.geog,5,2) as geography,
        sts.altitude AS altitude,
-       clu.collectionunitid as collectionunitid,
-       clu.collunitname AS collectionunit,
-       clu.handle AS handle,
-       cts.colltype AS unittype,
-       dts.datasetid AS datasetid,
-       dst.datasettype AS datasettype FROM
-ndb.datasets AS dts LEFT OUTER JOIN
-ndb.collectionunits AS clu ON clu.collectionunitid = dts.collectionunitid LEFT OUTER JOIN
-ndb.sites AS sts ON sts.siteid = clu.siteid  LEFT OUTER JOIN
-ndb.datasettypes as dst ON dst.datasettypeid = dts.datasettypeid LEFT OUTER JOIN
-ndb.collectiontypes as cts ON clu.colltypeid = cts.colltypeid
-WHERE
-  (${sitename} IS NULL OR sts.sitename LIKE ${sitename})
-  AND (${altmin} IS NULL OR sts.altitude >= ${altmin})
-  AND (${altmax} IS NULL OR sts.altitude <= ${altmax})
-  AND (${loc}    IS NULL OR ST_Intersects(ST_GeogFromText(${loc}), sts.geog))
-  AND (${siteid} IS NULL OR sts.siteid IN ${siteid})
-  AND sts.siteid IN (SELECT siteid FROM sitid)
+  	   json_agg(cus.collectionunit) AS collectionunits
+FROM
+   (SELECT * FROM collunit) AS cus
+   LEFT JOIN ndb.sites AS sts ON cus.siteid = sts.siteid
+GROUP BY sts.siteid
