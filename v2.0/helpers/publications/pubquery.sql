d20753d79893c85ced86a74debc09a8342b87681
Simon Goring <simon.j.goring@gmail.com>
2021-04-22 17:28:31 -0700

    Updating publication methods, and the API documentation in the swagger file.\nSwagger update is for the publications specifically.

--- v2.0/helpers/publications/pubquery.sql
+++ v2.0/helpers/publications/pubquery.sql
@@ -32,11 +32,13 @@ FROM ndb.publications AS pub
 INNER JOIN ndb.publicationauthors AS pa  ON pub.publicationid = pa.publicationid
 INNER JOIN ndb.contacts AS ca  ON      ca.contactid = pa.contactid
 LEFT JOIN ndb.datasetpublications AS dp ON dp.publicationid = pub.publicationid
+LEFT JOIN ndb.dslinks AS dsl ON dsl.datasetid = db.datasetid
 INNER JOIN ndb.publicationtypes AS pt  ON     pub.pubtypeid = pt.pubtypeid
 INNER JOIN ndb.pubtsv AS pts ON pts.publicationid = pub.publicationid
 WHERE
-  (${pubid}      IS NULL OR pub.publicationid = ANY (${pubid}::int[]))     AND
+  (${publicationid}      IS NULL OR pub.publicationid = ANY (${publicationid}::int[]))     AND
   (${datasetid}  IS NULL OR      dp.datasetid = ANY (${datasetid})) AND
+  (${siteid}     IS NULL OR      dsl.siteid = ANY (${siteid})) AND
   (${familyname} IS NULL OR     ca.familyname LIKE  ${familyname})  AND
   (${pubtype}    IS NULL OR        pt.pubtype =     ${pubtype})     AND
   (${year}       IS NULL OR          pub.year =     ${year})        AND
