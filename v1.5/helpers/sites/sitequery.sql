d651996a215479b67a2b36eaff496c8b54d781bd
Simon Goring <simon.j.goring@gmail.com>
2021-04-28 11:58:55 -0700

    Fixing an issue with site IDs in v1.5 queries

--- v1.5/helpers/sites/sitequery.sql
+++ v1.5/helpers/sites/sitequery.sql
@@ -1,17 +1,17 @@
-WITH sitid AS 
+WITH sitid AS
 (SELECT * FROM ndb.sitegeopolitical AS sgp WHERE
 (${gpid} IS NULL OR sgp.geopoliticalid = ${gpid}))
-SELECT sts.siteid AS siteid, 
+SELECT sts.siteid AS siteid,
        sts.sitename as sitename,
        sts.sitedescription AS sitedescription,
        ST_AsGeoJSON(sts.geog,5,2) as geography,
-       sts.altitude AS altitude, 
+       sts.altitude AS altitude,
        clu.collectionunitid as collectionunitid,
        clu.collunitname AS collectionunit,
        clu.handle AS handle,
-       cts.colltype AS unittype, 
+       cts.colltype AS unittype,
        dts.datasetid AS datasetid,
-       dst.datasettype AS datasettype FROM 
+       dst.datasettype AS datasettype FROM
 ndb.datasets AS dts LEFT OUTER JOIN
 ndb.collectionunits AS clu ON clu.collectionunitid = dts.collectionunitid LEFT OUTER JOIN
 ndb.sites AS sts ON sts.siteid = clu.siteid  LEFT OUTER JOIN
