d03ba713ecfa1144926ba768f9653d2298aae2b4
Simon Goring <simon.j.goring@gmail.com>
2021-10-15 11:38:36 -0700

    Fixing up the documentation and rebuilding the tests.

--- test/v2.0-data-dbtables-test.js
+++ test/v2.0-data-dbtables-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/dbtables', function () {
   describe('tests for get', function () {
     it('should respond 200 for "Returned table."', function () {
       var response = request('get', 'http://localhost:3005/v2.0/data/dbtables', {
-        'qs': { 'table': 'eu laborum', 'limit': 10, 'offset': 0 },
+        'qs': { 'table': 'quis', 'limit': 10, 'offset': 0 },
         'time': true
       });
 
