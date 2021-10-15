a7f0698acb98adb015dd35623b6b99293b7d84f7
Simon Goring <simon.j.goring@gmail.com>
2021-10-15 11:38:36 -0700

    Fixing up the documentation and rebuilding the tests.

--- test/v2.0-data-sites-{siteid}-contacts-test.js
+++ test/v2.0-data-sites-{siteid}-contacts-test.js
@@ -7,7 +7,7 @@ var expect = chakram.expect;
 describe('tests for /v2.0/data/sites/{siteid}/contacts', function () {
   describe('tests for get', function () {
     it('should respond 200 for "contact"', function () {
-      var response = request('get', 'http://localhost:3005/v2.0/data/sites/500/contacts', {
+      var response = request('get', 'http://localhost:3005/v2.0/data/sites/6005/contacts', {
         'time': true
       });
 
