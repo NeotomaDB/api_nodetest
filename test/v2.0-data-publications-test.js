336d5520cd3e8e0756edd578c2b2b0ac6d9a1b0e
SimonGoring <goring@wisc.edu>
2022-10-06 15:59:14 -0700

    Test file commits.

--- test/v2.0-data-publications-test.js
+++ test/v2.0-data-publications-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/publications', function () {
   describe('tests for get', function () {
     it('should respond 200 for "A list of publications."', function () {
       var response = request('get', 'http://localhost:3005/v2.0/data/publications', {
-        'qs': { 'publicationid': 15786, 'datasetid': 88953231, 'siteid': 27075, 'familyname': 'laboris proident', 'pubtype': 'Website', 'year': 1657, 'search': 'sint dolor esse est tempor', 'limit': 10, 'offset': 0 },
+        'qs': { 'publicationid': 1653, 'datasetid': 87558629, 'siteid': 8483, 'familyname': 'adipisicing quis', 'pubtype': 'Undergraduate thesis', 'year': 2034, 'search': 'eiusmod dolore ipsum commodo est', 'limit': 10, 'offset': 0 },
         'time': true
       });
 
