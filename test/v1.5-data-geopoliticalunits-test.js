466530ba5e3bea49cbbf7377c0a4d203ddd7385c
SimonGoring <goring@wisc.edu>
2022-10-06 13:27:12 -0700

    Ran the tests.

--- test/v1.5-data-geopoliticalunits-test.js
+++ test/v1.5-data-geopoliticalunits-test.js
@@ -8,7 +8,7 @@ describe('tests for /v1.5/data/geopoliticalunits', function () {
   describe('tests for get', function () {
     it('should respond 200 for "An array of geopolitical units."', function () {
       var response = request('get', 'http://localhost:3005/v1.5/data/geopoliticalunits', {
-        'qs': { 'gpid': 10474, 'gpname': 'elit in anim adipisicing', 'rank': 4, 'lower': false },
+        'qs': { 'gpid': 7562, 'gpname': 'officia et reprehenderit', 'rank': 1, 'lower': false },
         'time': true
       });
 
