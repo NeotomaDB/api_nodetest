99c8982f7c993766bb7b4d4241b2a9eabd0c4a1e
Simon Goring <simon.j.goring@gmail.com>
2022-11-15 13:43:27 -0800

    Adding the limit to the download function.

--- v2.0/helpers/download/downloadbydsid.sql
+++ v2.0/helpers/download/downloadbydsid.sql
@@ -1,1 +1,7 @@
-SELECT doi.doiapi(${datasetid}) AS data;
+SELECT doi.doiapi(${datasetid}) AS data
+OFFSET (CASE WHEN ${offset} IS NULL THEN 0
+                 ELSE ${offset}
+            END)
+LIMIT (CASE WHEN ${limit} IS NULL THEN 25
+            ELSE ${limit}
+        END);
