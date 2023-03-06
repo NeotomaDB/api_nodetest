d11a79695003f0653173aa1ab119fea5beab51d4
SimonGoring <goring@wisc.edu>
2023-03-06 10:14:07 -0800

    Fixed up the base search to include max ages
    I also sorted the query parameters, and removed tabs (replacing them with two spaces).

--- v2.0/helpers/datasets/datasetqueryfaster.sql
+++ v2.0/helpers/datasets/datasetqueryfaster.sql
@@ -6,28 +6,28 @@ WITH datasets AS (
     INNER JOIN ndb.constituentdatabases AS cdb ON cdb.databaseid = bigq.databaseid
     WHERE
     (${sitename} IS NULL OR bigq.sitename ILIKE ANY(${sitename}))
-	  AND (${altmin} IS NULL OR bigq.altitude >= ${altmin})
-	  AND (${altmax} IS NULL OR bigq.altitude <= ${altmax})
-	  AND (${loc}    IS NULL OR NOT ST_Disjoint(ST_GeogFromText(${loc})::geometry, bigq.geog::geometry))
-	  AND (${siteid} IS NULL OR bigq.siteid = ANY(${siteid}))
-	  AND (${gpid} IS NULL OR bigq.geopol && ${gpid})
-	  AND (${keywords} IS NULL OR bigq.keywords && ${keywords})
-	  AND (${contacts} IS NULL OR bigq.contacts && ${contacts})
-	  AND (${taxa} IS NULL OR bigq.taxa && ${taxa})
-    AND (${datasettype} IS NULL OR bigq.datasettype LIKE ${datasettype})
-    AND (${altmin} IS NULL OR    bigq.altitude > ${altmin})    
-    AND (${altmax} IS NULL OR    bigq.altitude < ${altmax})    
-    AND (${ageyoung} IS NULL OR     ${ageyoung} > bigq.younger)
-    AND (${ageold} IS NULL OR       ${ageold} < bigq.older)
     AND (${ageof} IS NULL OR        ${ageof} BETWEEN bigq.younger AND bigq.older)
-    AND (${doi} IS NULL OR dsdoi.doi = ANY(${doi}))
+    AND (${ageold} IS NULL OR       ${ageold} <= bigq.older)
+    AND (${ageyoung} IS NULL OR     ${ageyoung} >= bigq.younger)
+    AND (${altmax} IS NULL OR bigq.altitude <= ${altmax})
+    AND (${altmin} IS NULL OR bigq.altitude >= ${altmin})
+    AND (${contacts} IS NULL OR bigq.contacts && ${contacts})
     AND (${database} IS NULL OR cdb.databasename ILIKE ANY(${database}))
-    AND ((${datasetid}) IS NULL OR bigq.datasetid = ANY (${datasetid}))
+    AND (${datasetid} IS NULL OR bigq.datasetid = ANY (${datasetid}))
+    AND (${datasettype} IS NULL OR bigq.datasettype LIKE ${datasettype})
+    AND (${doi} IS NULL OR dsdoi.doi = ANY(${doi}))
+    AND (${gpid} IS NULL OR bigq.geopol && ${gpid})
+    AND (${keywords} IS NULL OR bigq.keywords && ${keywords})
+    AND (${loc}    IS NULL OR NOT ST_Disjoint(ST_GeogFromText(${loc})::geometry, bigq.geog::geometry))
+    AND (${maxage} IS NULL OR        ${maxage} >= bigq.younger)
+    AND (${minage} IS NULL OR        ${minage} <= bigq.older)
+    AND (${siteid} IS NULL OR bigq.siteid = ANY(${siteid}))
+    AND (${taxa} IS NULL OR bigq.taxa && ${taxa})
 ), dspiagg AS (
   SELECT
-	dts.datasetid AS datasetid,
-	sts.siteid AS siteid,
-	clu.collectionunitid,
+  dts.datasetid AS datasetid,
+  sts.siteid AS siteid,
+  clu.collectionunitid,
     jsonb_build_object('datasetid', dts.datasetid,
                            'datasettype', dst.datasettype,
                            'datasetnotes', dts.notes,
@@ -42,19 +42,19 @@ WITH datasets AS (
                                                                'ageold', agerange.older,
                                                                'units', agetypes.agetype)))
                                  AS dataset
-	FROM
+  FROM
     datasets AS ds 
-  	INNER JOIN ndb.datasets AS dts ON dts.datasetid = ds.datasetid
+    INNER JOIN ndb.datasets AS dts ON dts.datasetid = ds.datasetid
     LEFT OUTER JOIN ndb.datasetdoi           AS doi      ON ds.datasetid = doi.datasetid
-  	LEFT OUTER JOIN ndb.collectionunits      AS clu      ON clu.collectionunitid = dts.collectionunitid
-  	LEFT OUTER JOIN ndb.datasettypes         AS dst      ON dst.datasettypeid = dts.datasettypeid
-  	LEFT OUTER JOIN ndb.datasetdatabases     AS dsdb     ON dsdb.datasetid = dts.datasetid
-  	LEFT OUTER JOIN ndb.datasetpis           AS dspi     ON dspi.datasetid = dts.datasetid
-  	LEFT OUTER JOIN ndb.contacts             AS cnt      ON cnt.contactid = dspi.contactid
-  	LEFT OUTER JOIN ndb.dsageranges          AS agerange ON dts.datasetid = agerange.datasetid
-  	LEFT OUTER JOIN ndb.agetypes             AS agetypes ON agetypes.agetypeid = agerange.agetypeid
-  	LEFT OUTER JOIN ndb.constituentdatabases AS cstdb    ON dsdb.databaseid = cstdb.databaseid
-  	LEFT OUTER JOIN ndb.sites                AS sts      ON sts.siteid = clu.siteid
+    LEFT OUTER JOIN ndb.collectionunits      AS clu      ON clu.collectionunitid = dts.collectionunitid
+    LEFT OUTER JOIN ndb.datasettypes         AS dst      ON dst.datasettypeid = dts.datasettypeid
+    LEFT OUTER JOIN ndb.datasetdatabases     AS dsdb     ON dsdb.datasetid = dts.datasetid
+    LEFT OUTER JOIN ndb.datasetpis           AS dspi     ON dspi.datasetid = dts.datasetid
+    LEFT OUTER JOIN ndb.contacts             AS cnt      ON cnt.contactid = dspi.contactid
+    LEFT OUTER JOIN ndb.dsageranges          AS agerange ON dts.datasetid = agerange.datasetid
+    LEFT OUTER JOIN ndb.agetypes             AS agetypes ON agetypes.agetypeid = agerange.agetypeid
+    LEFT OUTER JOIN ndb.constituentdatabases AS cstdb    ON dsdb.databaseid = cstdb.databaseid
+    LEFT OUTER JOIN ndb.sites                AS sts      ON sts.siteid = clu.siteid
 GROUP BY
         clu.collectionunitid,
         dts.datasetid,
@@ -73,7 +73,7 @@ SELECT json_build_object(       'siteid', sts.siteid,
                         'collectionunit', clu.collunitname,
                                 'handle', clu.handle,
                               'unittype', cts.colltype,
-						'datasets', json_agg(dspi.dataset)) as site
+            'datasets', json_agg(dspi.dataset)) as site
 FROM dspiagg             AS dspi
   LEFT OUTER JOIN ndb.datasets AS dts ON dspi.datasetid = dts.datasetid
   LEFT OUTER JOIN ndb.collectionunits AS clu  ON clu.collectionunitid = dts.collectionunitid
@@ -84,5 +84,5 @@ OFFSET (CASE WHEN ${offset} IS NULL THEN 0
                  ELSE ${offset}
             END)
 LIMIT (CASE WHEN ${limit} IS NULL THEN 25
-			ELSE ${limit}
-		END);
+      ELSE ${limit}
+    END);
