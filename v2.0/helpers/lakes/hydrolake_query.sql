e61429de00612547f40473ba34ddb22d6143040d
Simon Goring <simon.j.goring@gmail.com>
2024-03-03 20:06:59 -0800

    Added a query for lake data from hydrolakes.
    The new endpoint (data/lakes) supports siteid search and buffer information. Used only in v2.

---
+++ v2.0/helpers/lakes/hydrolake_query.sql
@@ -0,0 +1,12 @@
+SELECT hl.objectid AS hydrolakeid,
+    hl.lake_name AS lakename,
+    hl.lake_type AS laketype,
+    hl.lake_area AS lakearea,
+    hl.shore_len AS shorelen,
+    hl.vol_total AS volume,
+    hl.depth_avg AS avgdepth,
+    hl.wshd_area AS watershedarea,
+    ST_ASTEXT(hl.shape) AS wkt_shape
+FROM ap.hydrolakes AS hl
+WHERE
+ST_DWITHIN((SELECT geog FROM ndb.sites WHERE siteid = ${siteid}), hl.shape::geography, ${buffer});
