78dc3fc483e8c8ffbb05869929fbc5979cb2df9c
SimonGoring <goring@wisc.edu>
2024-03-19 15:07:57 -0700

    Renamed the spatial geometry response, added swagger help.
    I updated the OpenAPI response for the faunal data layer. Will work to add a similar one for the lakes.
    I think I now want to change the call altogether, to have it be unified for lakes, fauna, plants/whatever and ice sheets.

--- v2.0/helpers/spatial/faunmap_geom.sql
+++ v2.0/helpers/spatial/faunmap_geom.sql
@@ -1,7 +1,7 @@
 SELECT array_agg(gid) AS gid,
        array_agg(spid) AS spid,
        array_agg(sciname) AS sciname,
-       ST_AsGeoJSON(ST_UNION(ST_ReducePrecision(ST_Transform(the_geom,3857),${prec}))) as the_geom 
+       ST_AsGeoJSON(ST_UNION(ST_ReducePrecision(ST_Transform(the_geom, 3857),${prec}))) as geom 
 FROM ap.faunranges 
 WHERE sciname ILIKE ${sciname} AND sciname IS NOT NULL
 LIMIT 1;
