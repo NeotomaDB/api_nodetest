2432c467c40883e56158b0e4d0bca7ee657f0cd6
SimonGoring <goring@wisc.edu>
2024-03-19 19:50:56 -0700

    Added support for the lakes APIs and fixed parameters elsewhere. Consistent precision support.
    Documentation in swagger now for both the lakes and faunal data, including parameters for precision (`prec`) and projection (`proj`).
    
    This meant some updates of the APIs and SQL queries, but all looks pretty clean now.

--- v2.0/helpers/spatial/faunmap_geom.sql
+++ v2.0/helpers/spatial/faunmap_geom.sql
@@ -1,7 +1,10 @@
 SELECT array_agg(gid) AS gid,
        array_agg(spid) AS spid,
        array_agg(sciname) AS sciname,
-       ST_AsGeoJSON(ST_UNION(ST_ReducePrecision(ST_Transform(the_geom, 3857),${prec}))) as geom 
+        ST_AsEWKT(
+            ST_Simplify(
+                ST_Transform(
+                    ST_UNION(the_geom), ${proj}),${prec}, false),
+        LENGTH(TRIM(TRAILING '0' FROM SPLIT_PART(${prec}::text, '.', 2)))) as geom 
 FROM ap.faunranges 
-WHERE sciname ILIKE ${sciname} AND sciname IS NOT NULL
-LIMIT 1;
+WHERE sciname ILIKE ${sciname} AND sciname IS NOT NULL;
