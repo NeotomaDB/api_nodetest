5e5ee842af8478a1c12e08dc61bd328901105197
Simon Goring <simon.j.goring@gmail.com>
2024-06-04 16:53:36 -0700

    Updating the dbsummary call to accept individual dbids
    The query returns the aggregated information for the db.

--- v2.0/helpers/landing/dbsummary.sql
+++ v2.0/helpers/landing/dbsummary.sql
@@ -1,18 +1,48 @@
-select cdb.databasename,
-  cdb.description,
-  cdb.url,
-  cnt.contactname,
-  cnt.email,
-  array_agg(distinct qt.datasettype) AS datasettypes,
-  MIN(qt.younger) as younger,
-  MAX(qt.older) as older,
-  COUNT(distinct qt.datasetid)::int AS datasets
-from ndb.constituentdatabases AS cdb
-left join ap.querytable as qt on cdb.databaseid = qt.databaseid
-left join ndb.contacts as cnt on cdb.contactid = cnt.contactid
-group by
-    cdb.databasename, 
+WITH constdb AS (
+  select
+    cdb.databaseid,
+    cdb.databasename,
     cdb.description,
-    cnt.contactname,
     cdb.url,
-    cnt.email;
+    cnt.contactname,
+    cnt.email,
+    COUNT(distinct qt.siteid)::int AS sites
+  from ndb.constituentdatabases AS cdb
+  left join ap.querytable as qt on cdb.databaseid = qt.databaseid
+  left join ndb.contacts as cnt on cdb.contactid = cnt.contactid
+  WHERE (cdb.databaseid IS NULL OR cdb.databaseid = ${dbid})
+  group by
+      cdb.databasename, 
+      cdb.databaseid,
+      cdb.description,
+      cdb.url,
+      cnt.contactname,
+	  cnt.email
+), datasets as (
+	select
+		qt.databaseid,
+		jsonb_build_object('datasettype', qt.datasettype,
+		  	'datasets', COUNT(distinct qt.datasetid)) as datasettypes
+	from ap.querytable as qt
+	where (qt.databaseid IS NULL OR qt.databaseid = ${dbid})
+	group by
+		qt.databaseid,
+		qt.datasettype
+)
+select
+  db.databaseid,
+  db.databasename,
+  db.url,
+  db.contactname,
+  db.email,
+  db.sites,
+  array_agg(dst.datasettypes) as datasettypes
+  from constdb as db
+  left join datasets as dst on dst.databaseid = db.databaseid
+  group by 
+	  db.databaseid,
+	  db.databasename,
+	  db.url,
+	  db.contactname,
+	  db.email,
+    db.sites;
