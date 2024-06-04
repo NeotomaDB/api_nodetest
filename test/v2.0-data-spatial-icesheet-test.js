0747261385651741d6dc40f8c55b4fc34cef6b6f
Simon Goring <simon.j.goring@gmail.com>
2024-06-04 16:58:50 -0700

    Updating the test files.

--- test/v2.0-data-spatial-icesheet-test.js
+++ test/v2.0-data-spatial-icesheet-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/spatial/icesheet', function() {
   describe('tests for get', function() {
     it('should respond 200 for "An object containing glacial extents for the selected time period (in **calibrated radiocarbon years**). "', function() {
       const response = request('get', 'http://localhost:3001/v2.0/data/spatial/icesheet', {
-        'qs': {'age': 16685, 'proj': 31668, 'prec': -5214491.139078572},
+        'qs': {'age': 5717, 'proj': 11503, 'prec': 24189594.679282982},
         'time': true,
       });
 
--- test/v2.0-data-taxa-test.js
+++ test/v2.0-data-taxa-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/taxa', function() {
   describe('tests for get', function() {
     it('should respond 200 for "A taxon or array of taxa."', function() {
       const response = request('get', 'http://localhost:3001/v2.0/data/taxa', {
-        'qs': {'taxonname': 'incididunt Excepteur eiusmod irure', 'taxongroup': 'veniam cillum culpa', 'ecolgroup': 'labore', 'status': 'false', 'limit': 10, 'offset': 0},
+        'qs': {'taxonname': 'et', 'taxongroup': 'aliquip Ut laboris minim', 'ecolgroup': 'anim et tempor consequat', 'status': '1', 'limit': 10, 'offset': 0},
         'time': true,
       });
 
