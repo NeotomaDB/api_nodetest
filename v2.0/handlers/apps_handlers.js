f0b9978c6528c8eb14bf118713cbe601b08cbf3a
Simon Goring <simon.j.goring@gmail.com>
2024-06-04 16:52:06 -0700

    Linting sites and the apps handler.

--- v2.0/handlers/apps_handlers.js
+++ v2.0/handlers/apps_handlers.js
@@ -30,175 +30,226 @@ module.exports = {
 // Defining the query functions:
 
 /* All the Endpoint functions */
-function collectiontypes (req, res, next) {
-  let db = req.app.locals.db
+/**
+ * Return metadata about the different collection types in Neotoma.
+ * @param {req} req - An express request object.
+ * @param {res} res - An express response object.
+ * @param {any} next - An express next object.
+ */
+function collectiontypes(req, res, next) {
+  const db = req.app.locals.db;
   db.query('select ap.getcollectiontypes()')
-    .then(function (data) {
-      res.status(200)
-        .type('application/json')
-        .jsonp({
-          status: 'success',
-          data: data,
-          message: 'Retrieved all collectiontypes'
-        })
-    })
-    .catch(function (err) {
-      res.status(500)
-        .json({
-          status: 'failure',
-          data: err.message
-        });
-    });
+      .then(function(data) {
+        res.status(200)
+            .type('application/json')
+            .jsonp({
+              status: 'success',
+              data: data,
+              message: 'Retrieved all collectiontypes',
+            });
+      })
+      .catch(function(err) {
+        res.status(500)
+            .json({
+              status: 'failure',
+              data: err.message,
+            });
+      });
 }
 
-function datasettypes (req, res, next) {
-  let db = req.app.locals.db
+/**
+ * Return metadata about the different dataset types in Neotoma.
+ * @param {req} req - An express request object.
+ * @param {res} res - An express response object.
+ * @param {any} next - An express next object.
+ */
+function datasettypes(req, res, next) {
+  const db = req.app.locals.db;
   db.query('select ap.getdatasettypes();')
-    .then(function (data) {
-      res.status(200)
-        .jsonp({
-          status: 'success',
-          data: data,
-          message: 'Retrieved all datasettypes'
-        })
-    })
-    .catch(function (err) {
-      res.status(500)
-        .json({
-          status: 'failure',
-          data: err.message
-        });
-    });
+      .then(function(data) {
+        res.status(200)
+            .jsonp({
+              status: 'success',
+              data: data,
+              message: 'Retrieved all datasettypes',
+            });
+      })
+      .catch(function(err) {
+        res.status(500)
+            .json({
+              status: 'failure',
+              data: err.message,
+            });
+      });
 }
 
-function taxaindatasets (req, res, next) {
-  let db = req.app.locals.db
+/**
+ * Return the list of taxa that are reported from all Neotoma datasets.
+ * @param {req} req - An express request object.
+ * @param {res} res - An express response object.
+ * @param {any} next - An express next object.
+ */
+function taxaindatasets(req, res, next) {
+  const db = req.app.locals.db;
   db.query('SELECT * FROM ap.taxaindatasetview;')
-    .then(function (data) {
-      res.status(200)
-        .type('application/json')
-        .jsonp({
-          status: 'success',
-          data: data,
-          message: 'Retrieved all taxa in datasets'
-        })
-    }).catch(function (err) {
-      res.status(500)
-        .json({
-          status: 'failure',
-          data: err.message
-        });
-    });
+      .then(function(data) {
+        res.status(200)
+            .type('application/json')
+            .jsonp({
+              status: 'success',
+              data: data,
+              message: 'Retrieved all taxa in datasets',
+            });
+      }).catch(function(err) {
+        res.status(500)
+            .json({
+              status: 'failure',
+              data: err.message,
+            });
+      });
 }
 
-function taxagrouptypes (req, res, next) {
-  let db = req.app.locals.db
+/**
+ * Return metadata about the different taxongroups used in Neotoma.
+ * @param {req} req - An express request object.
+ * @param {res} res - An express response object.
+ * @param {any} next - An express next object.
+ */
+function taxagrouptypes(req, res, next) {
+  const db = req.app.locals.db;
   db.query('select ap.gettaxagrouptypes();')
-    .then(function (data) {
-      res.status(200)
-        .jsonp({
-          status: 'success',
-          data: data,
-          message: 'Retrieved all taxagrouptypes'
-        })
-    })
-    .catch(function (err) {
-      res.status(500)
-        .json({
-          status: 'failure',
-          data: err.message
-        });
-    });
+      .then(function(data) {
+        res.status(200)
+            .jsonp({
+              status: 'success',
+              data: data,
+              message: 'Retrieved all taxagrouptypes',
+            });
+      })
+      .catch(function(err) {
+        res.status(500)
+            .json({
+              status: 'failure',
+              data: err.message,
+            });
+      });
 }
 
-function keywords (req, res, next) {
-  let db = req.app.locals.db
+/**
+ * Return the set of keywords reported in the Neotoma keywords table.
+ * @param {req} req - An express request object.
+ * @param {res} res - An express response object.
+ * @param {any} next - An express next object.
+ */
+function keywords(req, res, next) {
+  const db = req.app.locals.db;
   db.query('select ap.getkeywords();')
-    .then(function (data) {
-      res.status(200)
-        .jsonp({
-          status: 'success',
-          data: data,
-          message: 'Retrieved all keywords'
-        })
-    })
-    .catch(function (err) {
-      res.status(500)
-        .json({
-          status: 'failure',
-          data: err.message
-        });
-    });
+      .then(function(data) {
+        res.status(200)
+            .jsonp({
+              status: 'success',
+              data: data,
+              message: 'Retrieved all keywords',
+            });
+      })
+      .catch(function(err) {
+        res.status(500)
+            .json({
+              status: 'failure',
+              data: err.message,
+            });
+      });
 }
 
-function authorpis (req, res, next) {
-  let db = req.app.locals.db
+/**
+ * Return the full list of Neotoma PIs assigned to databases.
+ * @param {req} req - An express request object.
+ * @param {res} res - An express response object.
+ * @param {any} next - An express next object.
+ */
+function authorpis(req, res, next) {
+  const db = req.app.locals.db;
   db.query('select ap.getpeople();')
-    .then(function (data) {
-      res.status(200)
-        .jsonp({
-          status: 'success',
-          data: data,
-          message: 'Retrieved all authors PIs'
-        })
-    })
-    .catch(function (err) {
-      res.status(500)
-        .json({
-          status: 'failure',
-          data: err.message
-        });
-    });
+      .then(function(data) {
+        res.status(200)
+            .jsonp({
+              status: 'success',
+              data: data,
+              message: 'Retrieved all authors PIs',
+            });
+      })
+      .catch(function(err) {
+        res.status(500)
+            .json({
+              status: 'failure',
+              data: err.message,
+            });
+      });
 }
 
-function taphonomysystems (req, res, next) {
-  let db = req.app.locals.db
+/**
+ * Return metadata about the different taphonomic systems reported for a
+ * dataset type.
+ * @param {req} req - An express request object.
+ * @param {res} res - An express response object.
+ * @param {any} next - An express next object.
+ */
+function taphonomysystems(req, res, next) {
+  const db = req.app.locals.db;
   // Get the query string:
-  var datasettypeid = req.query.datasettypeid;
+  const datasettypeid = req.query.datasettypeid;
 
   if (!datasettypeid) {
     res.status(200)
-      .jsonp({
-        status: 'failure',
-        data: null,
-        message: 'No datasetTypeId provided.'
-      })
+        .jsonp({
+          status: 'failure',
+          data: null,
+          message: 'No datasetTypeId provided.',
+        });
   } else {
-    db.query('select ap.gettaphonomicsystems(' + datasettypeid + ');')
-      .then(function (data) {
-        res.status(200)
-          .jsonp({
-            status: 'success',
-            data: data,
-            message: 'Retrieved taphonomic system for dataset type id'
-          })
-      })
-      .catch(function (err) {
-        res.status(500)
-          .json({
-            status: 'failure',
-            data: err.message
-          });
-      });
+    db.query('select ap.gettaphonomicsystems(${dsty});',
+        {'dsty': datasettypeid})
+        .then(function(data) {
+          res.status(200)
+              .jsonp({
+                status: 'success',
+                data: data,
+                message: 'Retrieved taphonomic system for dataset type id',
+              });
+        })
+        .catch(function(err) {
+          res.status(500)
+              .json({
+                status: 'failure',
+                data: err.message,
+              });
+        });
   }
 }
 
-function depositionalenvironments (req, res, next) {
-  let db = req.app.locals.db
+/**
+ * Return metadata about the different depositional environments
+ * reported in Neotoma.
+ * @param {req} req - An express request object.
+ * @param {res} res - An express response object.
+ * @param {any} next - An express next object.
+ */
+function depositionalenvironments(req, res, next) {
+  const db = req.app.locals.db;
   db.query('select ap.getdeptenvtypesroot();')
-    .then(function (data) {
-      res.status(200)
-        .jsonp({
-          status: 'success',
-          data: data,
-          message: 'Retrieved root depositional environment types'
-        })
-    })
-    .catch(function (err) {
-      res.status(500)
-        .json({
-          status: 'failure',
-          data: err.message
-        });
-    });
+      .then(function(data) {
+        res.status(200)
+            .jsonp({
+              status: 'success',
+              data: data,
+              message: 'Retrieved root depositional environment types',
+            });
+      })
+      .catch(function(err) {
+        res.status(500)
+            .json({
+              status: 'failure',
+              data: err.message,
+            });
+      });
 }
