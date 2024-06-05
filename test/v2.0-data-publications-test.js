012499a3d36e234033731f179309b3e8398407fb
Simon Goring <simon.j.goring@gmail.com>
2024-06-04 18:01:59 -0700

    Updating tests.

--- test/v2.0-data-publications-test.js
+++ test/v2.0-data-publications-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/publications', function() {
   describe('tests for get', function() {
     it('should respond 200 for "A list of publications."', function() {
       const response = request('get', 'http://localhost:3001/v2.0/data/publications', {
-        'qs': {'publicationid': 19608, 'datasetid': 60011899, 'siteid': 28654, 'familyname': 'mollit dolor', 'pubtype': 'Website', 'year': 1505, 'search': 'exercitation esse', 'limit': 10, 'offset': 0},
+        'qs': {'publicationid': 15375, 'datasetid': 91452635, 'siteid': 4701, 'familyname': 'eu ipsum mollit', 'pubtype': 'Master\'s Thesis', 'year': 1736, 'search': 'nostrud sunt deserunt', 'limit': 10, 'offset': 0},
         'time': true,
       });
 
