6ad7de2bd3d7261a28ea729801de06d361c713b3
Simon <simon.j.goring@gmail.com>
2025-02-06 13:28:39 -0800

    Added more documentation, along with new route for contact ORCID.
    Still fixing `use-strict` changes and adding more documentation to the API to help clena things up.

--- v2.0/helpers/contacts/contacts.js
+++ v2.0/helpers/contacts/contacts.js
@@ -1,6 +1,6 @@
 // Contacts query:
-
-const { sql, validateOut } = require('../../../src/neotomaapi.js');
+'use strict';
+const {sql, validateOut} = require('../../../src/neotomaapi.js');
 
 // Create a QueryFile globally, once per file:
 const contactbyid = sql('../v2.0/helpers/contacts/contactbyid.sql');
@@ -8,16 +8,24 @@ const contactquery = sql('../v2.0/helpers/contacts/contactquery.sql');
 const contactbydsid = sql('../v2.0/helpers/contacts/contactbydsid.sql');
 const contactbystid = sql('../v2.0/helpers/contacts/contactbysiteid.sql');
 
-function contacts (req, res, next) {
-  let db = req.app.locals.db
-  var contactIdUsed = !!req.query.contactid;
+/**
+ * General call to obtain contact information from the Neotoma Database.
+ * @param {req} req An express.js `requests` object.
+ * @param {res} res An express.js `response` object.
+ * @param {next} next An express.js `next` object.
+ */
+function contacts(req, res, next) {
+  const db = req.app.locals.db;
+  const contactIdUsed = !!req.query.contactid;
+  let contactid;
+
   if (contactIdUsed) {
-    var contactid = String(req.query.contactid).split(',').map(function (item) {
+    contactid = String(req.query.contactid).split(',').map(function(item) {
       return parseInt(item, 10);
     });
   };
 
-  var outobj = {
+  let outobj = {
     'contactid': contactid,
     'contactname': req.query.contactname,
     'familyname': req.query.familyname,
@@ -25,199 +33,211 @@ function contacts (req, res, next) {
     'name': req.query.name,
     'similarity': req.query.similarity,
     'limit': req.query.limit || 25,
-    'offset': req.query.offset || 0
+    'offset': req.query.offset || 0,
   };
 
   outobj = validateOut(outobj);
 
-  if (Object.keys(outobj).every(function (x) { return typeof outobj[x] === 'undefined'; }) === false) {
+  if (Object.keys(outobj).every(function(x) {return typeof outobj[x] === 'undefined';}) === false) {
     db.any(contactquery, outobj)
-      .then(function (data) {
-        if (data.length === 0) {
+        .then(function(data) {
+          if (data.length === 0) {
           // We're returning the structure, but nothing inside it:
+            var returner = [];
+          } else {
+            returner = data;
+          };
+
+          res.status(200)
+              .json({
+                status: 'success',
+                data: returner,
+              });
+        })
+        .catch(function(err) {
+          res.status(500)
+              .json({
+                status: 'failure',
+                data: err.message,
+              });
+        });
+  };
+}
+
+/**
+ * Specific call to obtain contact information from the Neotoma Database
+ * using a contact ID.
+ * @param {req} req An express.js `requests` object.
+ * @param {res} res An express.js `response` object.
+ * @param {next} next An express.js `next` object.
+ */
+function contactsbyid(req, res, next) {
+  const db = req.app.locals.db;
+  const contactUsed = !!req.params.contactid;
+  let contactid;
+
+  if (contactUsed) {
+    contactid = String(req.params.contactid).split(',').map(function(item) {
+      return parseInt(item, 10);
+    });
+  } else {
+    res.status(500)
+        .json({
+          status: 'failure',
+          data: null,
+          message: 'Must pass either queries or an integer sequence.',
+        });
+  }
+
+  db.any(contactbyid, [contactid])
+      .then(function(data) {
+        let returner = [];
+        if (data.length !== 0) {
+          returner = data;
+        };
+
+        res.status(200)
+            .json({
+              status: 'success',
+              data: returner,
+              message: 'Retrieved all tables',
+            });
+      })
+      .catch(function(err) {
+        res.status(500)
+            .json({
+              status: 'failure',
+              data: err.message,
+            });
+      });
+}
+
+/**
+ * Specific call to obtain contact information from the Neotoma Database
+ * using a dataset ID.
+ * @param {req} req An express.js `requests` object.
+ * @param {res} res An express.js `response` object.
+ * @param {next} next An express.js `next` object.
+ */
+function contactsbydataid(req, res, next) {
+  const db = req.app.locals.db;
+  const datasetIdUsed = !!req.params.datasetid;
+  let datasetid;
+  if (datasetIdUsed) {
+    datasetid = String(req.params.datasetid).split(',').map(function(item) {
+      return parseInt(item, 10);
+    });
+  } else {
+    res.status(500)
+        .json({
+          status: 'failure',
+          data: null,
+          message: 'Must pass either queries or an integer sequence.',
+        });
+  }
+
+  db.any(contactbydsid, [datasetid])
+      .then(function(data) {
+        if (data.length === 0) {
+        // We're returning the structure, but nothing inside it:
           var returner = [];
         } else {
           returner = data;
         };
 
         res.status(200)
-          .json({
-            status: 'success',
-            data: returner
-          });
+            .json({
+              status: 'success',
+              data: returner,
+              message: 'Retrieved all tables',
+            });
       })
-      .catch(function (err) {
+      .catch(function(err) {
         res.status(500)
-          .json({
-            status: 'failure',
-            data: err.message
-          });
+            .json({
+              status: 'failure',
+              data: err.message,
+            });
       });
-  };
 }
 
-function contactsbyid (req, res, next) {
-  let db = req.app.locals.db
-  var contactUsed = !!req.params.contactid
-
-  if (contactUsed) {
-    var contactid = String(req.params.contactid).split(',').map(function (item) {
-      return parseInt(item, 10);
-    });
-  } else {
-    res.status(500)
-      .json({
-        status: 'failure',
-        data: null,
-        message: 'Must pass either queries or an integer sequence.'
-      });
-  }
-
-  db.any(contactbyid, [contactid])
-    .then(function (data) {
-      if (data.length === 0) {
-        // We're returning the structure, but nothing inside it:
-        var returner = [];
-      } else {
-        returner = data;
-      };
-
-      res.status(200)
-        .json({
-          status: 'success',
-          data: returner,
-          message: 'Retrieved all tables'
-        });
-    })
-    .catch(function (err) {
-      res.status(500)
-        .json({
-          status: 'failure',
-          data: err.message
-        });
-    });
-}
-
-function contactsbydataid (req, res, next) {
-  let db = req.app.locals.db
-  var datasetIdUsed = !!req.params.datasetid
-
-  if (datasetIdUsed) {
-    var datasetid = String(req.params.datasetid).split(',').map(function (item) {
-      return parseInt(item, 10);
-    });
-  } else {
-    res.status(500)
-      .json({
-        status: 'failure',
-        data: null,
-        message: 'Must pass either queries or an integer sequence.'
-      });
-  }
-
-  db.any(contactbydsid, [datasetid])
-    .then(function (data) {
-      if (data.length === 0) {
-        // We're returning the structure, but nothing inside it:
-        var returner = [];
-      } else {
-        returner = data;
-      };
-
-      res.status(200)
-        .json({
-          status: 'success',
-          data: returner,
-          message: 'Retrieved all tables'
-        });
-    })
-    .catch(function (err) {
-      res.status(500)
-        .json({
-          status: 'failure',
-          data: err.message
-        });
-    });
-}
-
-function contactclassbydsid (req, res, next) {
-
+function contactclassbydsid(req, res, next) {
   const contactclassbydsid = sql('../v2.0/helpers/contacts/contact_class.sql');
-  let db = req.app.locals.db
-  var datasetIdUsed = !!req.params.datasetid;
+  const db = req.app.locals.db;
+  const datasetIdUsed = !!req.params.datasetid;
   if (datasetIdUsed) {
-    var datasetid = parseInt(req.params.datasetid)
+    var datasetid = parseInt(req.params.datasetid);
   } else {
     res.status(500)
-      .json({
-        status: 'failure',
-        data: null,
-        message: 'Must pass either queries or an integer sequence.'
-      });
+        .json({
+          status: 'failure',
+          data: null,
+          message: 'Must pass either queries or an integer sequence.',
+        });
   }
   db.any(contactclassbydsid, [datasetid])
-    .then(function (data) {
-      if (data.length === 0) {
+      .then(function(data) {
+        if (data.length === 0) {
         // We're returning the structure, but nothing inside it:
-        var returner = [];
-      } else {
-        returner = data;
-      };
+          var returner = [];
+        } else {
+          returner = data;
+        };
 
-      res.status(200)
-        .json({
-          status: 'success',
-          data: returner,
-          message: 'Retrieved all tables'
-        });
-    })
-    .catch(function (err) {
-      res.status(500)
-        .json({
-          status: 'failure',
-          data: err.message
-        });
-    });
+        res.status(200)
+            .json({
+              status: 'success',
+              data: returner,
+              message: 'Retrieved all tables',
+            });
+      })
+      .catch(function(err) {
+        res.status(500)
+            .json({
+              status: 'failure',
+              data: err.message,
+            });
+      });
 }
 
-function contactsbysiteid (req, res, next) {
-  let db = req.app.locals.db
-  var siteIdUsed = !!req.params.siteid;
+function contactsbysiteid(req, res, next) {
+  const db = req.app.locals.db;
+  const siteIdUsed = !!req.params.siteid;
   if (siteIdUsed) {
-    var siteid = String(req.params.siteid).split(',').map(function (item) {
+    var siteid = String(req.params.siteid).split(',').map(function(item) {
       return parseInt(item, 10);
     });
   } else {
     res.status(500)
-      .json({
-        status: 'failure',
-        data: null,
-        message: 'Must pass either queries or an integer sequence.'
-      });
-  }
-  db.any(contactbystid, [siteid])
-    .then(function (data) {
-      if (data.length === 0) {
-        // We're returning the structure, but nothing inside it:
-        var returner = [];
-      } else {
-        returner = data;
-      };
-
-      res.status(200)
-        .json({
-          status: 'success',
-          data: returner,
-          message: 'Retrieved all tables'
-        });
-    })
-    .catch(function (err) {
-      res.status(500)
         .json({
           status: 'failure',
-          data: err.message
+          data: null,
+          message: 'Must pass either queries or an integer sequence.',
         });
-    });
+  }
+  db.any(contactbystid, [siteid])
+      .then(function(data) {
+        if (data.length === 0) {
+        // We're returning the structure, but nothing inside it:
+          var returner = [];
+        } else {
+          returner = data;
+        };
+
+        res.status(200)
+            .json({
+              status: 'success',
+              data: returner,
+              message: 'Retrieved all tables',
+            });
+      })
+      .catch(function(err) {
+        res.status(500)
+            .json({
+              status: 'failure',
+              data: err.message,
+            });
+      });
 }
 
 module.exports.contactquery = contacts;
