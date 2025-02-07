e2a5f8806472bbcb30660611989c1affcf5cfce2
Simon <simon.j.goring@gmail.com>
2025-02-07 15:02:36 -0800

    Updating the tests and results.

--- test/v2.0-data-publications-test.js
+++ test/v2.0-data-publications-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/publications', function() {
   describe('tests for get', function() {
     it('should respond 200 for "A list of publications."', function() {
       const response = request('get', 'http://localhost:3001/v2.0/data/publications', {
-        'qs': {'publicationid': 4797, 'datasetid': 21741750, 'siteid': 25253, 'familyname': 'mollit anim nostrud', 'pubtype': 'Legacy', 'year': 1592, 'search': 'exercitation aute amet sunt', 'limit': 10, 'offset': 0},
+        'qs': {'publicationid': 9855, 'datasetid': 71099666, 'siteid': 2113, 'familyname': 'Duis laboris', 'pubtype': 'Journal Article', 'year': 1953, 'search': 'est fugiat laboris', 'limit': 10, 'offset': 0},
         'time': true,
       });
 
