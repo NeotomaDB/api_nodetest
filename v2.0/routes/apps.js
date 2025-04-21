447ac474edae380f45446f2ca480d4a2b0176b9f
hoffmanick <134728053+hoffmanick@users.noreply.github.com>
2025-04-21 12:51:34 -0700

    external taxon query
    made a new api that returns all the relevant external taxa links for an input taxon id

--- v2.0/routes/apps.js
+++ v2.0/routes/apps.js
@@ -35,11 +35,9 @@ router.get('/constdb/datasetuploads', handlers.datasetuploads);
 router.get('/constdb/datasetages', handlers.datasetranges);
 router.get('/constdb', handlers.databasesummaries);
 router.get('/contactsummary/:datasetid', handlers.contactoverview);
-<<<<<<< Updated upstream
 router.get('/orcids/contact', handlers.contact_orcid);
 router.get('/orcids/orcid', handlers.orcid_contact);
-=======
 router.get('/datasetpi',handlers.datasetsbypi);
->>>>>>> Stashed changes
+router.get('/exttax',handlers.externaltaxonquery);
 
 module.exports = router;
