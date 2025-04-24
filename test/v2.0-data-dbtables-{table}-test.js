8e6a83bd4d7ca4dfd68f2515dfb707a4faf6e2ed
Simon <simon.j.goring@gmail.com>
2025-04-24 11:06:06 -0700

    Updating tests.

--- test/v2.0-data-dbtables-{table}-test.js
+++ test/v2.0-data-dbtables-{table}-test.js
@@ -7,8 +7,8 @@ const expect = chakram.expect;
 describe('tests for /v2.0/data/dbtables/{table}', function() {
   describe('tests for get', function() {
     it('should respond 200 for "Returned table."', function() {
-      const response = request('get', 'http://localhost:3001/v2.0/data/dbtables/aliquadoloreoccaecatfugiat', {
-        'qs': {'count': true, 'limit': 10, 'offset': 0},
+      const response = request('get', 'http://localhost:3001/v2.0/data/dbtables/dolore', {
+        'qs': {'count': false, 'limit': 10, 'offset': 0},
         'time': true,
       });
 
--- test/v2.0-data-publications-test.js
+++ test/v2.0-data-publications-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/publications', function() {
   describe('tests for get', function() {
     it('should respond 200 for "A list of publications."', function() {
       const response = request('get', 'http://localhost:3001/v2.0/data/publications', {
-        'qs': {'publicationid': 6751, 'datasetid': 9813654, 'siteid': 16178, 'familyname': 'enim', 'pubtype': 'Master\'s Thesis', 'year': 1723, 'search': 'pariatur adipisicing Duis dolor', 'limit': 10, 'offset': 0},
+        'qs': {'publicationid': 11347, 'datasetid': 4641500, 'siteid': 18742, 'familyname': 'pariatur', 'pubtype': 'Edited Book', 'year': 1884, 'search': 'aliquip sunt consequat', 'limit': 10, 'offset': 0},
         'time': true,
       });
 
