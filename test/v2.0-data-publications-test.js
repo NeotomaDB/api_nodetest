7ace5aee21041a0963b4f6da81d97b03f7c786ed
Simon <simon.j.goring@gmail.com>
2025-05-14 10:21:10 -0700

    Running tests. WKT still runs slow.

--- test/v2.0-data-publications-test.js
+++ test/v2.0-data-publications-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/publications', function() {
   describe('tests for get', function() {
     it('should respond 200 for "A list of publications."', function() {
       const response = request('get', 'http://localhost:3001/v2.0/data/publications', {
-        'qs': {'publicationid': 13844, 'datasetid': 81642774, 'siteid': 2757, 'familyname': '%mddtelXci', 'pubtype': 'Authored Book', 'year': 1972, 'search': 'elit aute laboris anim officia', 'limit': 10, 'offset': 0},
+        'qs': {'publicationid': 12263, 'datasetid': 18998057, 'siteid': 43549, 'familyname': 'bU HaylUmk', 'pubtype': 'Edited Book', 'year': 1994, 'search': 'non esse', 'limit': 10, 'offset': 0},
         'time': true,
       });
 
--- test/v2.0-data-taxa-test.js
+++ test/v2.0-data-taxa-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/taxa', function() {
   describe('tests for get', function() {
     it('should respond 200 for "A taxon or array of taxa."', function() {
       const response = request('get', 'http://localhost:3001/v2.0/data/taxa', {
-        'qs': {'taxonname': 'magna quis', 'taxagroup': 'Duis dolor sit cillum ut', 'ecolgroup': 'aute Ut voluptate laborum eiusmod', 'status': 0, 'limit': 10, 'offset': 0},
+        'qs': {'taxonname': 'tempor sint', 'taxagroup': 'Excepteur eiusmod', 'ecolgroup': 'labore fugiat mollit', 'status': true, 'limit': 10, 'offset': 0},
         'time': true,
       });
 
