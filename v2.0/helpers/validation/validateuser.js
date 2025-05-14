5f0e6cad90fdff6a2c15c8d73bda02ec4575fecb
Simon <simon.j.goring@gmail.com>
2025-05-14 10:14:18 -0700

    Merged datasetPIs into production.

--- v2.0/helpers/validation/validateuser.js
+++ v2.0/helpers/validation/validateuser.js
@@ -6,18 +6,14 @@
 // TOKEN_SECRET.
 // The user token will expire in 1 week.
 
-// Require Neo4j
-const jwt = require('jsonwebtoken');
-const fetch = require('node-fetch');
-
 /**
  * Check an ORCID token passed to the Neotoma API:
  * @param {object} req An object passed through Express
  * @param {object} res A resolve object passed through Express
- * @param {object} next A next object passed through Express */
+ * @param {object} next A next object passed through Express *
+ * **/
 function checktoken(req, res, next) {
   const token = req.body.token;
-  console.log(token);
   const agent = 'Neotoma Paleoecology Database Authentication goring@wisc.edu';
   // We want to take in a token and check that we get something reasonable back.
   fetch('https://orcid.org/oauth/userinfo', {
@@ -37,32 +33,23 @@ function checktoken(req, res, next) {
         return (JSON.parse(out));
       })
       .then((data) => {
-        console.log(data)
         if (Object.keys(data).includes('error')) {
-          const msg = 'The ORCID token passed to Throughput is not valid:' +
+          const msg = 'The ORCID token passed to Neotoma is not valid:' +
             token;
           res.status(407)
               .json({
                 status: 'Proxy Authentication Required',
                 data: data,
-                message: msg
+                message: msg,
               });
         } else {
-          const ndbtoken = jwt.sign({
-            orcid: data,
-          },
-          process.env.TOKEN_SECRET,
-          {
-            expiresIn: '1w',
-          });
           res.status(200)
               .json({
                 status: 'success',
                 data: {
-                  token: ndbtoken,
                   user: data,
                 },
-                message: 'Neotoma token expires in 1wk'
+                message: 'Neotoma token expires in 1wk',
               });
         }
       })
