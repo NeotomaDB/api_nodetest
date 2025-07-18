18f30289c1a0e57279ddfa24602abe1257d04d46
Simon <simon.j.goring@gmail.com>
2025-07-18 11:43:02 -0700

    Tests and results.

--- test/v2.0-data-contacts-test.js
+++ test/v2.0-data-contacts-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/contacts', function() {
   describe('tests for get', function() {
     it('should respond 200 for "contact"', function() {
       const response = request('get', 'http://localhost:3001/v2.0/data/contacts', {
-        'qs': {'contactid': 2440, 'familyname': 'WNzPwcZ', 'contactname': 'e', 'contactstatus': 'inactive', 'limit': 10, 'offset': 0},
+        'qs': {'contactid': 6720, 'familyname': 'SVHa', 'contactname': 'aVZW', 'contactstatus': 'active', 'limit': 10, 'offset': 0},
         'time': true,
       });
 
--- test/v2.0-data-publications-test.js
+++ test/v2.0-data-publications-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/publications', function() {
   describe('tests for get', function() {
     it('should respond 200 for "A list of publications."', function() {
       const response = request('get', 'http://localhost:3001/v2.0/data/publications', {
-        'qs': {'publicationid': 15306, 'datasetid': 66384220, 'siteid': 3806, 'familyname': '%tTmM', 'pubtype': 'Edited Report', 'year': 1771, 'search': 'non mollit nisi', 'limit': 10, 'offset': 0},
+        'qs': {'publicationid': 2884, 'datasetid': 57669249, 'siteid': 47534, 'familyname': 'Yrsd', 'pubtype': 'Undergraduate thesis', 'year': 1560, 'search': 'aliqua ad non', 'limit': 10, 'offset': 0},
         'time': true,
       });
 
