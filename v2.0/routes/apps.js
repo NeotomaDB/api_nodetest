cf4e73ffd70744ea2a0921ea10263edb67f95a3b
Simon <simon.j.goring@gmail.com>
2025-07-18 11:39:13 -0700

    Adding the NativeLands route to protect api key secrets.

--- v2.0/routes/apps.js
+++ v2.0/routes/apps.js
@@ -35,10 +35,14 @@ router.get('/constdb/datasetuploads', handlers.datasetuploads);
 router.get('/constdb/datasetages', handlers.datasetranges);
 router.get('/constdb', handlers.databasesummaries);
 router.get('/contactsummary/:datasetid', handlers.contactoverview);
+// Gives the orcid by the contactid.
 router.get('/orcids/contact', handlers.contact_orcid);
+// Gives the contact with an orcid.
 router.get('/orcids/orcid', handlers.orcid_contact);
 router.get('/datasetpi', handlers.datasetsbypi);
 router.get('/exttax', handlers.externaltaxonquery);
 router.post('/orcids/validate', handlers.validateusers);
+router.get('/contacts/:contactid/datasets', handlers.contact_datasets);
+router.get('/nativelands', handlers.sendNativeLands);
 
 module.exports = router;
