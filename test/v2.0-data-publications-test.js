46d1dc23e408904ada5f24a8aa0a5634bdbf0a00
Simon <simon.j.goring@gmail.com>
2025-08-07 10:49:06 -0700

    Ran tests.

--- test/v2.0-data-publications-test.js
+++ test/v2.0-data-publications-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/publications', function() {
   describe('tests for get', function() {
     it('should respond 200 for "A list of publications."', function() {
       const response = request('get', 'http://localhost:3001/v2.0/data/publications', {
-        'qs': {'publicationid': 9765, 'datasetid': 89892361, 'siteid': 40716, 'familyname': 'PaX', 'pubtype': 'Other Edited', 'year': 1701, 'search': 'quis deserunt aliquip', 'limit': 10, 'offset': 0},
+        'qs': {'publicationid': 7157, 'datasetid': 51176253, 'siteid': 30018, 'familyname': 'FrBL', 'pubtype': 'Other Authored', 'year': 1679, 'search': 'exercitation consectetur', 'limit': 10, 'offset': 0},
         'time': true,
       });
 
