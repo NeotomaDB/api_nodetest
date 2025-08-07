37b1febf79c57f951d78d15dacc8116ed3842a1e
Ondřej Mottl <ondrej.mottl@gmail.com>
2025-08-07 12:48:58 -0700

    depenv: add tests

--- test/v2.0-data-contacts-test.js
+++ test/v2.0-data-contacts-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/contacts', function() {
   describe('tests for get', function() {
     it('should respond 200 for "contact"', function() {
       const response = request('get', 'http://localhost:3001/v2.0/data/contacts', {
-        'qs': {'contactid': 2440, 'familyname': 'WNzPwcZ', 'contactname': 'e', 'contactstatus': 'inactive', 'limit': 10, 'offset': 0},
+        'qs': {'contactid': 3467, 'familyname': 'ItyGta', 'contactname': 'poRe Peuci ', 'contactstatus': 'extant', 'limit': 10, 'offset': 0},
         'time': true,
       });
 
