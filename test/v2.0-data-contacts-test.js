33d4f09c852c273b40e12ba26b9765cf33bcdad1
Simon <simon.j.goring@gmail.com>
2025-08-07 10:49:06 -0700

    Ran tests.

--- test/v2.0-data-contacts-test.js
+++ test/v2.0-data-contacts-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/contacts', function() {
   describe('tests for get', function() {
     it('should respond 200 for "contact"', function() {
       const response = request('get', 'http://localhost:3001/v2.0/data/contacts', {
-        'qs': {'contactid': 8621, 'familyname': 'cn', 'contactname': 'IRvy', 'contactstatus': 'inactive', 'limit': 10, 'offset': 0},
+        'qs': {'contactid': 8211, 'familyname': 'oEwFQkILDM', 'contactname': 'LYh', 'contactstatus': 'retired', 'limit': 10, 'offset': 0},
         'time': true,
       });
 
