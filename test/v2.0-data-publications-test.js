abc05360d75e223bcadfb70bbef9fe371afc8115
Simon <simon.j.goring@gmail.com>
2025-08-07 16:27:48 -0700

    Cleaned up tests.

--- test/v2.0-data-publications-test.js
+++ test/v2.0-data-publications-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/publications', function() {
   describe('tests for get', function() {
     it('should respond 200 for "A list of publications."', function() {
       const response = request('get', 'http://localhost:3001/v2.0/data/publications', {
-        'qs': {'publicationid': 13401, 'datasetid': 41997086, 'siteid': 27135, 'familyname': 'IQIJldec', 'pubtype': 'Website', 'year': 1970, 'search': 'amet dolor in dolore Lorem', 'limit': 10, 'offset': 0},
+        'qs': {'publicationid': 19430, 'datasetid': 73200495, 'siteid': 44421, 'familyname': 'PAcXip%G', 'pubtype': 'Legacy', 'year': 1975, 'search': 'cillum dolor cupidatat elit exercitation', 'limit': 10, 'offset': 0},
         'time': true,
       });
 
