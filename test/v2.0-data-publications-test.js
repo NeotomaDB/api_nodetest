61fdab4ca6c4ecf6b1c4964db2cf4f77cf2c0b25
Simon <simon.j.goring@gmail.com>
2025-09-03 11:53:05 -0700

    Updated tests & test outcomes.

--- test/v2.0-data-publications-test.js
+++ test/v2.0-data-publications-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/publications', function() {
   describe('tests for get', function() {
     it('should respond 200 for "A list of publications."', function() {
       const response = request('get', 'http://localhost:3001/v2.0/data/publications', {
-        'qs': {'publicationid': 7709, 'datasetid': 65885320, 'siteid': 7942, 'familyname': 'RkY', 'pubtype': 'Undergraduate thesis', 'year': 1890, 'search': 'fugiat Duis officia tempor minim', 'limit': 10, 'offset': 0},
+        'qs': {'publicationid': 17707, 'datasetid': 1620567, 'siteid': 38529, 'familyname': 'rxhPHz', 'pubtype': 'Edited Book', 'year': 1924, 'search': 'sit voluptate eiusmod occaecat Excepteur', 'limit': 10, 'offset': 0},
         'time': true,
       });
 
