602ce5c36db0e82cc9f53fe4a904e9562d4e1ca3
Simon <simon.j.goring@gmail.com>
2025-10-20 11:42:55 -0700

    Adding all the stuff back up.

--- test/v2.0-data-publications-test.js
+++ test/v2.0-data-publications-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/publications', function() {
   describe('tests for get', function() {
     it('should respond 200 for "A list of publications."', function() {
       const response = request('get', 'http://localhost:3001/v2.0/data/publications', {
-        'qs': {'publicationid': 5759, 'datasetid': 54134460, 'siteid': 26239, 'familyname': 'TgdGr', 'pubtype': 'Journal Article', 'year': 1852, 'search': 'ipsum minim do cupidatat', 'limit': 10, 'offset': 0},
+        'qs': {'publicationid': 19332, 'datasetid': 5521658, 'siteid': 10564, 'familyname': 'Q', 'pubtype': 'Legacy', 'year': 1878, 'search': 'voluptate Ut ut Lorem', 'limit': 10, 'offset': 0},
         'time': true,
       });
 
--- test/v2.0-data-spatial-lakes-test.js
+++ test/v2.0-data-spatial-lakes-test.js
@@ -8,7 +8,7 @@ describe('tests for /v2.0/data/spatial/lakes', function() {
   describe('tests for get', function() {
     it('should respond 200 for "An object containing all matched lakes within some buffer distance of a site. Data derived from the [HydroLakes database](https://www.hydrosheds.org/products/hydrolakes):<br> * Messager, M.L., Lehner, B., Grill, G., Nedeva, I., Schmitt, O. (2016). Estimating the volume and age of water stored in global lakes using a geo-statistical approach. *Nature Communications*, 7: 13603. doi: [10.1038/ncomms13603](https://doi.org/10.1038/ncomms13603) "', function() {
       const response = request('get', 'http://localhost:3001/v2.0/data/spatial/lakes', {
-        'qs': {'siteid': 19384, 'buffer': 6172, 'prec': 1000, 'proj': 4326},
+        'qs': {'siteid': 4897, 'buffer': 9192, 'prec': 1000, 'proj': 4326},
         'time': true,
       });
 
