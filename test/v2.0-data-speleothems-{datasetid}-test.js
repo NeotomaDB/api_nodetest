887eaa27e53ad2769df4f464230a6cd019397b1b
Socorro Dominguez <seiryu88@icloud.com>
2025-10-21 10:56:11 -0700

    API-42 Updated open-api documentation and yml file for speleothem searches

--- test/v2.0-data-speleothems-{datasetid}-test.js
+++ test/v2.0-data-speleothems-{datasetid}-test.js
@@ -7,7 +7,7 @@ const expect = chakram.expect;
 describe('tests for /v2.0/data/speleothems/{datasetid}', function() {
   describe('tests for get', function() {
     it('should respond 200 for "Metadata associated with speleothems submitted through SISAL."', function() {
-      const response = request('get', 'http://localhost:3001/v2.0/data/speleothems/69617,69611', {
+      const response = request('get', 'http://localhost:3001/v2.0/data/speleothems/{datasetid}', {
         'time': true,
       });
 
