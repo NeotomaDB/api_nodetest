7b821a190194f17ae6515ebc095faf99b4d51cb1
Simon <simon.j.goring@gmail.com>
2026-01-27 14:06:16 -0800

    Updating eslint and tests to relect new packate updates
    The openapi stuff seems to have broken with some of the package updates.

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "eslint/config";
+
+import js from "@eslint/js";
+
+export default defineConfig([
+	{
+		files: ["**/*.js"],
+		plugins: {
+			js,
+		},
+		extends: ["js/recommended"],
+		rules: {
+			"no-unused-vars": "warn",
+			"no-undef": "warn",
+		},
+	},
+]);
